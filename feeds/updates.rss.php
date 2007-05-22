e6d20ee831fc4c345505064eb4b549416a0fa802
misterhaan <misterhaan@users.noreply.github.com>
2007-05-22 01:36:55 +0000

    update feeds to use auFeed

--- feeds/updates.rss.php
+++ feeds/updates.rss.php
@@ -1,32 +1,14 @@
 <?
   require_once  dirname($_SERVER['DOCUMENT_ROOT']) . '/lib/track7.php';
-  header('Content-Type: application/rss+xml; charset=utf-8');
-  echo '<?xml version="1.0" encoding="utf-8"?>' . "\n";
-?>
-<rss version="2.0">
-  <channel>
-    <title>track7 updates</title>
-    <link>http://<?=$_SERVER['HTTP_HOST']; ?>/new.php</link>
-    <description>track7 site updates feed</description>
-    <language>en-us</language>
-    <copyright>copyright 2007 track7</copyright>
-    <generator>PHP/<?=phpversion(); ?></generator>
-    <docs>http://blogs.law.harvard.edu/tech/rss</docs>
+  require_once 'auFeed.php';
+  $rss = new auFeed();
+  $rss->Start('track7 updates', '/new.php', 'track7 site updates feed', 'copyright 2007 track7');
 
-<?
   $updates = 'select id, instant, `change` from updates order by instant desc';
   if($updates = $db->GetLimit($updates, 0, 15, '', ''))
     while($update = $updates->NextRecord()) {
       $update->change = str_replace('href="/', 'href="http://' . $_SERVER['HTTP_HOST'] . '/', $update->change); 
-?>
-    <item>
-      <description><![CDATA[<?=$update->change; ?>]]></description>
-      <pubDate><?=gmdate('r', $update->instant); ?></pubDate>
-      <guid isPermaLink="false">update<?=$update->id; ?></guid>
-    </item>
-
-<?
+      $rss->AddItem($update->change, '', '', $update->instant, 'update' . $update->id);
     }
+  $rss->End();
 ?>
-  </channel>
-</rss>
