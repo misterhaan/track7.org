5b1fded54d55734d6066096268905293ce9b9313
misterhaan <misterhaan@users.noreply.github.com>
2007-05-22 17:04:57 +0000

    update feeds to use changed auFeed

--- feeds/posts.rss.php
+++ feeds/posts.rss.php
@@ -1,8 +1,7 @@
 <?
   require_once  dirname($_SERVER['DOCUMENT_ROOT']) . '/lib/track7.php';
   require_once 'auFeed.php';
-  $rss = new auFeed();
-  $rss->Start('track7 forum posts', '/oi/', 'all posts from the track7 forums', 'copyright 2007 track7');
+  $rss = new auFeed('track7 forum posts', '/oi/', 'all posts from the track7 forums', 'copyright 2007 track7');
 
   $posts = 'select p.id, p.number, p.tid, t.fid, p.subject, p.post, f.title as forum, p.uid, u.login, p.instant from oiposts as p left join oithreads as t on p.tid=t.id left join oiforums as f on t.fid=f.id left join users as u on p.uid=u.uid order by p.instant desc';
   if($posts = $db->GetLimit($posts, 0, 15, '', ''))
