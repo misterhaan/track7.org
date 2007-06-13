28c0e3358d9de9b36e7ba6bc2a5095b6d9adb621
misterhaan <misterhaan@users.noreply.github.com>
2007-06-13 05:30:54 +0000

    add tag editing, tagged photo album fixes

---
+++ tools/taginfo.php
@@ -0,0 +1,26 @@
+<?
+  require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/lib/track7.php';
+  if(!$user->GodMode) {
+    header('HTTP/1.0 403 Forbidden');
+    @include $_SERVER['DOCUMENT_ROOT'] . '/403.php';
+    die;
+  }
+  if(strlen($_GET['name']) && strlen($_GET['type'])) {
+    $desc = 'select description from taginfo where type=\'' . addslashes($_GET['type']) . '\' and name=\'' . addslashes($_GET['name']) . '\'';
+    if($desc = $db->GetValue($desc, 'error looking up tag information', 'tag not found', true)) {
+      require_once 'auForm.php';
+      require_once 'auText.php';
+      $tagedit = new auForm('tagedit', '?type=' . $_GET['type'] . '&amp;name=' . $_GET['name']);
+      $tagedit->AddField('desc', 'description', 'enter a description for this tag', false, auText::HTML2BB($desc), _AU_FORM_FIELD_BBCODE);
+      $tagedit->AddButtons('save', 'save tag description');
+      if($tagedit->CheckInput(true)) {
+        $update = 'update taginfo set description=\'' . addslashes(auText::BB2HTML($_POST['desc'])) . '\' where type=\'' . addslashes($_GET['type']) . '\' and name=\'' . addslashes($_GET['name']) . '\'';
+        if(false !== $db->Change($update, 'error updating tag description'))
+          $page->Info('tag description successfully updated');
+      }
+      $tagedit->WriteHTML(true);
+    }
+  }
+  $page->Start('tag information editor', 'tag information editor');
+  $page->End();
+?>
