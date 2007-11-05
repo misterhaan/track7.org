5c6e9039c7dc3b0221fb0dc64c0cadf89661c745
misterhaan <misterhaan@4369b48c-882d-0410-aabb-eb458be0e6f5>
2007-11-05 18:35:59 +0000

    update browser issues page

--- live/browsers.php
+++ live/browsers.php
@@ -4,20 +4,21 @@
 ?>
       <p>
         track7 uses cascading style sheets (css) to present its layout and
-        style.&nbsp; this works great in browsers such as mozilla firefox and 
-        recent versions of netscape, but internet explorer and other older
-        browsers tend to have problems with parts of it.&nbsp; often all this
-        means is that people visiting track7 with internet explorer will miss
-        out on some of the more advanced features.&nbsp; track7 should look
-        &quot;good enough&quot; in any recent browser or internet explorer -- if
-        you have problems, try upgrading to one of the following; each of which
-        track7 is tested in, and any issues found are listed below:
+        style.&nbsp; this works great in browsers such as firefox and opera, but 
+        internet explorer and other older browsers tend to have problems with
+        parts of it.&nbsp; often all this means is that people visiting track7
+        with internet explorer will miss out on some of the more advanced
+        features.&nbsp; track7 should look &ldquo;good enough&rdquo; in any
+        recent browser.&nbsp; if you have problems, try upgrading to one of the
+        following (preferably not internet explorer, however).&nbsp; track7 is
+        tested in each of them, and any issues found but not worked around are
+        listed below:
       </p>
       <table class="text" cellspacing="0">
         <thead class="minor"><tr><th>browser</th><th>version</th><th title="the engine actually displays web pages, and may be used by other browsers as well">engine</th></tr></thead>
         <tbody>
-          <tr class="firstchild"><td><a href="http://www.mozilla.org/products/firefox/">mozilla firefox</a></td><td>2.0.0.3</td><td>gecko</td></tr>
-          <tr><td><a href="http://www.opera.com/download/">opera</a></td><td>9.10</td><td>opera</td></tr>
+          <tr class="firstchild"><td><a href="http://www.mozilla.org/products/firefox/">mozilla firefox</a></td><td>2.0.0.9</td><td>gecko</td></tr>
+          <tr><td><a href="http://www.opera.com/download/">opera</a></td><td>9.24</td><td>opera</td></tr>
           <tr><td><a href="http://www.microsoft.com/windows/ie/default.mspx?mg_ID=10010">microsoft internet explorer</a></td><td>7.0</td><td>mshtml</td></tr>
           <tr><td><a href="http://www.konqueror.org/">konqueror</a></td><td>3.5</td><td>khtml</td></tr>
         </tbody>
@@ -41,9 +42,7 @@
 ?>
       <p>
         based on the user agent sent by your browser, you appear to be using the
-        <abbr title="<?=htmlspecialchars($_SERVER['HTTP_USER_AGENT']); ?>"><?=$browser; ?> browser, based on the <?=$engine; ?> engine</abbr>.&nbsp;
-        note that this value is not reliable, and if it's not entirely correct,
-        parts of track7 may not look right.
+        <abbr title="<?=htmlspecialchars($_SERVER['HTTP_USER_AGENT']); ?>"><?=$browser; ?> browser, based on the <?=$engine; ?> engine</abbr>.
       </p>
 
 <?
