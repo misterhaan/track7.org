c67a93d79000e97051dbd87575756ee911d52f86
misterhaan <misterhaan@4369b48c-882d-0410-aabb-eb458be0e6f5>
2007-11-13 04:03:50 +0000

    rss feed for bln entries

---
+++ live/feeds/entries.rss.php
@@ -0,0 +1,26 @@
+<?
+  require_once  dirname($_SERVER['DOCUMENT_ROOT']) . '/lib/track7.php';
+  require_once 'auFeed.php';
+  $rss = new auFeed('bln entries', '/comments.php', 'all entries posted on bln at track7', 'copyright 2007 track7');
+
+  if($_GET['tags'])
+    if(substr($_GET['tags'], 0, 1) == '-') {
+      $tags = explode(',', substr($_GET['tags'], 1));
+      foreach($tags as $tag)
+        $entries .= ' tags=\'' . $tag . '\' or tags like \'%,' . $tag . '\' or tags like \'' . $tag . ',%\' or tags like \'%,' . $tag . ',%\'';
+      $entries = 'select name, instant, tags, title, post from bln where not (' . $entries . ') order by instant desc';
+    } else {
+      $tags = explode(',', $_GET['tags']);
+      foreach($tags as $tag)
+        $entries .= ' tags=\'' . $tag . '\' or tags like \'%,' . $tag . '\' or tags like \'' . $tag . ',%\' or tags like \'%,' . $tag . ',%\'';
+      $entries = 'select name, instant, tags, title, post from bln where' . $entries . ' order by instant desc';
+    }
+  else
+    $entries = 'select name, instant, tags, title, post from bln order by instant desc';
+  if($entries = $db->GetLimit($entries, 0, 15, '', ''))
+    while($entry = $entries->NextRecord()) {
+      $entry->post = str_replace('href="/', 'href="http://' . $_SERVER['HTTP_HOST'] . '/', $entry->post);
+      $rss->AddItem('<p>' . $entry->post . '</p>', $entry->title, 'http://' . $_SERVER['HTTP_HOST'] . '/output/pen/bln/' . $entry->name, $entry->instant, 'http://' . $_SERVER['HTTP_HOST'] . '/output/pen/bln/' . $entry->name, true);
+    }
+  $rss->End();
+?>
