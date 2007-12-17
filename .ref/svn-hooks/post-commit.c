95fa509ea634ea5da5451f1bf5ee0b7f4b3e5fb3
misterhaan <misterhaan@users.noreply.github.com>
2007-12-17 14:45:59 +0000

    remove .ref

--- .ref/svn-hooks/post-commit.c
+++
@@ -1,8 +0,0 @@
-#define REAL_SCRIPT "/home/misterhaan/svn/track7/hooks/post-commit.bash"
-#include <sys/types.h>
-#include <unistd.h>
-main( ac, av )
-	  char **av;
-{
-	  execv( REAL_SCRIPT, av );
-}
