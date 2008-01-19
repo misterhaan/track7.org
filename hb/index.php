82335c521fb29fb2885c097268992f8259277c4f
misterhaan <misterhaan@users.noreply.github.com>
2008-01-19 00:53:25 +0000

    start on new tagged forums

---
+++ hb/index.php
@@ -0,0 +1,40 @@
+<?
+  require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/lib/track7.php';
+  require_once 'hb.inc';
+
+  $page->Start('threads - holla back', 'holla back thread listing');
+  if(strlen($_GET['tag'])) {
+    $page->ShowTagDescription('posts', addslashes($_GET['tag']));
+    $threads = ' where t.tags=\'' . addslashes($_GET['tag']) . '\' or t.tags like \'' . addslashes($_GET['tag']) . ',%\' or t.tags like \'%,' . addslashes($_GET['tag']) . '\' or t.tags like \'%,' . addslashes($_GET['tag']) . ',%\'';
+  } else
+    $page->TagCloud('posts');
+  // DO:  add post new thread link
+  $threads = 'select t.id, t.tags, t.title, t.instant, tu.login, t.posts, p.subject, p.instant as pinstant, pu.login as plogin from hbthreads as t left join users as tu on tu.uid=t.uid left join hbposts as p on p.id=t.lastpost left join users as pu on pu.uid=p.uid' . $threads . ' order by p.instant desc';
+  if($threads = $db->GetSplit($threads, 20, 0, '', '', 'error looking up threads', 'no threads found')) {
+?>
+      <table class="text" id="hbthreadlist" cellspacing="0">
+        <thead class="minor"><tr><th>thread</th><th>tags</th><th>posts</th><th>started</th><th>last post</th></tr></thead>
+        <tbody>
+<?
+    while($thread = $threads->NextRecord()) {
+      // show list of threads
+      $subject = html_entity_decode($thread->subject, ENT_COMPAT, _CHARSET);
+      if(strlen($subject) > 16)
+        $subject = substr($subject, 0, 15) . '...';
+      $subject = htmlentities($subject, ENT_COMPAT, _CHARSET);
+?>
+          <tr><td><a href="/hb/thread<?=$thread->id; ?>/"><?=$thread->title; ?></a></td><td class="detail"><?=HB::TagLinks($thread->tags); ?></td><td class="number"><?=$thread->posts; ?></td><td class="detail"><?=$user->tzdate('Y-m-d g:i a', $thread->instant) . ' by ' . ($thread->login ? $thread->login : 'anonymous'); ?></td><td class="detail"><a href="/hb/thread<?=$thread->id; ?>#p<?=$thread->lastpost; ?>"<?=$subject != $thread->subject ? ' title="' . $thread->subject . '"' : ''; ?>><?=$subject; ?></a> <?=$user->tzdate('Y-m-d g:i a', $thread->pinstant) . ' by ' . ($thread->plogin ? $thread->plogin : 'anonymous'); ?></td></tr>
+<?
+    }
+?>
+        </tbody>
+        <tfoot><tr><td colspan="5">
+<?
+      $page->SplitLinks();
+?>
+        </td></tr></tfoot>
+      </table>
+<?
+  }
+  $page->End();
+?>
---
+++ hb/thread.php
@@ -0,0 +1,104 @@
+<?
+  require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/lib/track7.php';
+  require_once 'hb.inc';
+
+  // look up thread
+  if(is_numeric($_GET['id'])) {
+    $thread = 'select id, tags, title from hbthreads where id=\'' . addslashes($_GET['id']) . '\'';
+	  if($thread = $db->GetRecord($thread, 'error looking up thread', 'thread not found')) {
+	  	$page->Start($thread->title . ' - holla back', $thread->title, 'tags:&nbsp; ' . HB::TagLinks($thread->tags));
+	  	$posts = 'select u.login, s.rank, p.id, p.uid, p.instant, p.subject, p.post, p.history, r.signature, r.avatar, c.flags&' . _FLAG_USERCONTACT_SHOWEMAIL . ' as showemail, c.email, c.website, f.frienduid from hbposts as p left join users as u on p.uid=u.uid left join usercontact as c on u.uid=c.uid left join userstats as s on u.uid=s.uid left join userprofiles as r on u.uid=r.uid left join userfriends as f on f.frienduid=u.uid and f.fanuid=\'' . $user->ID . '\' where p.thread=\'' . $thread->id . '\' order by instant';
+	  	if($posts = $db->GetSplit($posts, _FORUM_POSTS_PER_PAGE, 0, '', '', 'error getting posts for this thread', 'this thread is empty!', true, true)) {
+        while($post = $posts->NextRecord()) {
+?>
+      <table class="post" cellspacing="0"><tr>
+        <td class="userinfo">
+<?
+          if($post->uid > 0) {
+?>
+          <a href="/user/<?=$post->login; ?>/"><?=$post->login; ?></a>
+<?
+            if($post->avatar) {
+?>
+          <img class="avatar" alt="" src="/user/avatar/<?=$post->login; ?>.<?=$post->avatar; ?>" />
+<?
+            }
+?>
+          <div class="frequency" title="frequency"><?=$post->rank; ?></div>
+<?
+          } else {
+?>
+          anonymous
+
+<?
+          }
+?>
+        </td>
+        <td>
+          <div class="head">
+            <div class="subject"><a class="ref" id="p<?=$post->id; ?>" href="#p<?=$post->id; ?>">subject:</a>&nbsp; <span class="response"><?=$post->subject; ?></span></div>
+            <div class="time">posted:&nbsp; <span class="response"><?=strtolower($user->tzdate('g:i:s a, M d, Y', $post->instant)); ?></span></div>
+          </div>
+          <p>
+            <?=$post->post; ?>
+
+          </p>
+<?
+          if($post->history) {
+?>
+          <p class="history">
+<?=substr($post->history, 0, strpos($post->history, "\r") ? -8 : -7); ?>
+
+          </p>
+<?
+	        }
+	        if($post->uid > 0 && $post->signature) {
+?>
+          <p class="signature">
+            <?=$post->signature; ?>
+
+          </p>
+<?
+          }
+?>
+          <div class="foot">
+<?
+	        if($post->uid > 0) {
+?>
+            <div class="userlinks">
+              <a href="/user/sendmessage.php?to=<?=$post->login; ?>" title="send <?=$post->login; ?> a private message">pm</a>
+<?
+	          if($post->showemail)
+	            echo '              | <a href="mailto:' . TEXT::safemail($post->email) . '" title="send ' . $post->login . ' an e-mail">e-mail</a>' . "\n";
+	          if($post->website)
+	            echo '              | <a href="' . $post->website . '" title="visit ' . $post->login . '\'s website">www</a>' . "\n";
+	          if($user->Valid && !$post->frienduid)
+	            echo '              | <a href="/user/friends.php?add=' . $post->login . '" title="add ' . $post->login . ' to your friend list">+friend</a>' . "\n";
+?>
+            </div>
+<?
+          }
+          if($user->Valid && $user->id == $post->uid || $user->GodMode) {
+?>
+            <a href="/hb/thread<?=$thread->id; ?>/edit=<?=$post->id; ?>" title="edit the above post">edit</a> |
+            <a href="/hb/thread<?=$thread->id; ?>/delete=<?=$post->id; ?>" title="delete the above post">delete</a> |
+<?
+          }
+?>
+            <a href="/hb/thread<?=$thread->id; ?>/reply<?=$post->id; ?>" title="quote the above post in a new reply">reply</a>
+          </div>
+        </td>
+      </tr></table>
+
+<?
+        }
+?>
+      <p><a href="/hb/thread<?=$thread->id; ?>/reply">add a reply</a></p>
+<?
+        $page->SplitLinks();
+      }
+	  	$page->End();
+	  }
+  }
+  $page->Show404();
+?>
