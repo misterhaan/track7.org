99046c6eb5a2ed3a87b0ec44885bcc4786618219
misterhaan <misterhaan@4369b48c-882d-0410-aabb-eb458be0e6f5>
2008-05-14 20:24:44 +0000

    support full-width styles again

---
+++ live/style/fullwidth.css.php
@@ -0,0 +1,15 @@
+<?
+  require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/lib/style.track7.php';
+?>
+/******************************************************************************\
+ * track7 style sheet by misterhaan of http://www.track7.org/                 *
+ *                                                                            *
+ * -> '<?=STYLE; ?>' layout style for browser issues page                            <? for($i = strlen(STYLE); $i < 5; $i++) echo ' '; ?>*
+\******************************************************************************/
+
+html {
+  background: none;
+}
+body {
+  max-width: none;
+}
