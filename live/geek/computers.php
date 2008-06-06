7d459b4374445eabaf00e3a40d85f061247cf83b
misterhaan <misterhaan@4369b48c-882d-0410-aabb-eb458be0e6f5>
2008-06-06 03:08:46 +0000

    update tesla hardware

--- live/geek/computers.php
+++ live/geek/computers.php
@@ -8,9 +8,9 @@
         <tr class="first"><th>name</th><td>hecubus</td></tr>
         <tr><th>class</th><td>workstation</td></tr>
         <tr><th>purpose</th><td>file / ftp / http server / dvr</td></tr>
-        <tr><th>mainboard</th><td>gigabyte ga-6vxe7+</td></tr>
-        <tr><th>processor</th><td>intel pentium iii 933 MHz</td></tr>
-        <tr><th>ram</th><td>256 MB pc133 sdram<br />128 MB pc133 sdram</td></tr>
+        <tr><th>mainboard</th><td><!-- gigabyte ga-945gzm-s2 -->gigabyte ga-6vxe7+</td></tr>
+        <tr><th>processor</th><td><!-- intel pentium d 2.66 GHz -->intel pentium iii 933 MHz</td></tr>
+        <tr><th>ram</th><td><!-- g.skill 1 GB pc2 5300 ddr2 sdram -->256 MB pc133 sdram<br />128 MB pc133 sdram</td></tr>
         <tr><th>video</th><td>matrox mystique 220 pci (4 MB)</td></tr>
         <tr><th>tuner</th><td>hauppage wintv-pvr 150</td></tr>
         <tr><th>network</th><td>belkin pci 10/100 ethernet</td></tr>
@@ -28,18 +28,17 @@
         <tr class="first"><th>name</th><td>tesla</td></tr>
         <tr><th>class</th><td>workstation</td></tr>
         <tr><th>purpose</th><td>main workstation / gaming</td></tr>
-        <tr><th>mainboard</th><td>gigabyte ga-945gzm-s2</td></tr>
-        <tr><th>processor</th><td>2.66-ghz intel pentium d</td></tr>
-        <tr><th>ram</th><td>1 GB pc2 5300 ddr2 sdram</td></tr>
+        <tr><th>mainboard</th><td>gigabyte ga-ep35-ds3l</td></tr>
+        <tr><th>processor</th><td>intel core 2 quad q9300 2.5 GHz</td></tr>
+        <tr><th>ram</th><td>g.skill 1 GB pc2 8500 ddr2 sdram<br />g.skill 1 GB pc2 8500 ddr2 sdram</td></tr>
         <tr><th>video</th><td>pny verto nvidia geforce 7300 gt (256 MB)</td></tr>
-        <tr><th>audio</th><td>creative x-fi xtreme gamer<br />realtek high definition audio (onboard)</td></tr>
+        <tr><th>audio</th><td>realtek high definition audio (onboard)</td></tr>
         <tr><th>tuner</th><td>easy tv</td></tr>
         <tr><th>network</th><td>realtek rtl8169 1000/100/10 ethernet (onboard)</td></tr>
         <tr><th>reader</th><td>rosewill multi card reader</td></tr>
-        <tr><th>ide0</th><td>western digital caviar 80 GB wd800jb</td></tr>
-        <tr><th>ide1</th><td>lg 8/4/24/4/2/16/12/32x dvd+/-rw</td></tr>
-        <tr><th>ide2</th><td>samsung dvd burner (sata)</td></tr>
-        <tr><th>operating system</th><td>ubuntu 8.04 hardy heron<br />microsoft windows vista</td></tr>
+        <tr><th>sata0</th><td>western digital caviar 80 GB wd800jd</td></tr>
+        <tr><th>sata1</th><td>samsung dvd burner</td></tr>
+        <tr><th>operating system</th><td>ubuntu 8.04 hardy heron 64-bit<br />microsoft windows vista 32-bit</td></tr>
         <tr><th>keyboard</th><td>logitech premium desktop</td></tr>
         <tr><th>mouse</th><td>logitech mx revolution<br />logitech cordless optical</td></tr>
         <tr><th>monitor</th><td>samsung syncmaster 204b</td></tr>
@@ -64,7 +63,7 @@
         <tr><th>reader</th><td>sd card reader</td></tr>
         <tr><th>ide0</th><td>hitachi 100 GB 5k100</td></tr>
         <tr><th>ide1</th><td>matshita dvd-ram</td></tr>
-        <tr><th>operating system</th><td>microsoft windows vista<br />ubuntu 7.10 gutsy gibbon</td></tr>
+        <tr><th>operating system</th><td>microsoft windows vista 32-bit<br />ubuntu 8.04 gutsy gibbon 32-bit</td></tr>
         <tr><th>keyboard</th><td>toshiba satellite r25</td></tr>
         <tr><th>mouse</th><td>logitech v450 wireless laser</td></tr>
         <tr><th>monitor</th><td>toshiba wide 14.1″</td></tr>
