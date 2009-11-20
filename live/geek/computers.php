e9af3c4d157f9ce4e88e1cb9c39f829b8d795e79
misterhaan <misterhaan@4369b48c-882d-0410-aabb-eb458be0e6f5>
2009-11-20 21:16:06 +0000

    update windows and ubuntu versions; remove tesla tv card and update hard drive

--- live/geek/computers.php
+++ live/geek/computers.php
@@ -17,6 +17,7 @@
         <tr><th>network</th><td>realtek 8110sc 10/100/1000 ethernet (onboard)</td></tr>
         <tr><th>ide0</th><td>western digital caviar 80 GB wd800jb</td></tr>
         <tr><th>ide1</th><td>iomega zip100</td></tr>
+        <!-- western digital caviar 80 GB wd800jd -->
         <tr><th>sata2</th><td>western digital caviar 400 GB wd4000aaks</td></tr>
         <tr><th>sata3</th><td>lite-on dvd-rom dh-16d2s-04</td></tr>
         <tr><th>operating system</th><td>fedora 11 64-bit</td></tr>
@@ -34,12 +35,11 @@
         <tr><th>ram</th><td>g.skill 1 GB pc2 8500 ddr2 sdram<br />g.skill 1 GB pc2 8500 ddr2 sdram<br />g.skill 1 GB pc2 8500 ddr2 sdram<br />g.skill 1 GB pc2 8500 ddr2 sdram</td></tr>
         <tr><th>video</th><td>evga 384-p3-n851-ar nvidia geforce 8800 gs (384 MB)</td></tr>
         <tr><th>audio</th><td>realtek high definition audio (onboard)</td></tr>
-        <tr><th>tuner</th><td>easy tv</td></tr>
         <tr><th>network</th><td>realtek rtl8169 1000/100/10 ethernet (onboard)</td></tr>
         <tr><th>reader</th><td>rosewill multi card reader</td></tr>
-        <tr><th>sata0</th><td>western digital caviar 80 GB wd800jd</td></tr>
+        <tr><th>sata0</th><td>western digital velociraptor 150 GB wd1500hlfs</td></tr>
         <tr><th>sata1</th><td>samsung dvd burner sh-s183l</td></tr>
-        <tr><th>operating system</th><td>ubuntu 9.04 jaunty jackalope 64-bit<br />microsoft windows 7 rc 64-bit</td></tr>
+        <tr><th>operating system</th><td>ubuntu 9.10 intrepid ibex 64-bit<br />microsoft windows 7 64-bit</td></tr>
         <tr><th>keyboard</th><td>logitech premium desktop</td></tr>
         <tr><th>mouse</th><td>logitech mx revolution<br />logitech cordless optical</td></tr>
         <tr><th>monitor</th><td>samsung syncmaster 204b</td></tr>
@@ -64,7 +64,7 @@
         <tr><th>reader</th><td>sd card reader</td></tr>
         <tr><th>ide0</th><td>hitachi 100 GB 5k100</td></tr>
         <tr><th>ide1</th><td>matshita dvd-ram</td></tr>
-        <tr><th>operating system</th><td>ubuntu 9.04 jaunty jackalope 32-bit<br />microsoft windows 7 rc 32-bit</td></tr>
+        <tr><th>operating system</th><td>ubuntu 9.10 intrepid ibex 32-bit<br />microsoft windows 7 32-bit</td></tr>
         <tr><th>keyboard</th><td>toshiba satellite r25</td></tr>
         <tr><th>mouse</th><td>logitech v450 wireless laser</td></tr>
         <tr><th>monitor</th><td>toshiba wide 14.1″</td></tr>
