93101f7c08ad488f48a27f0dc0e1b4680ce671d9
misterhaan <misterhaan@users.noreply.github.com>
2010-05-04 18:48:55 +0000

    exclude mobile domain from spiders

--- robots.php
+++ robots.php
@@ -1,6 +1,6 @@
 <?
   header('Content-Type: text/plain');
-  if(strpos($_SERVER['HTTP_HOST'], 'track7.org') === false) {
+  if(strpos($_SERVER['HTTP_HOST'], 'track7.org') === false || strpos($_SERVER['HTTP_HOST'], 'm.track7.org') !== false) {
     header('Last-Modified: ' . gmdate('D, j M Y H:i:s \G\M\T', filemtime($_SERVER['DOCUMENT_ROOT'] . '/' . $_SERVER['PHP_SELF'])));
 ?>
 User-Agent: *
