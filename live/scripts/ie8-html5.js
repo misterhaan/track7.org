82d3c5b0350335d82a4925c7617dafd3e3911622
misterhaan <misterhaan@4369b48c-882d-0410-aabb-eb458be0e6f5>
2011-01-05 17:15:09 +0000

    switch to html5

---
+++ live/scripts/ie8-html5.js
@@ -0,0 +1,5 @@
+// create html5 elements so ie8 and previous will apply styles
+document.createElement("header");
+document.createElement("nav");
+document.createElement("article");
+document.createElement("footer");
