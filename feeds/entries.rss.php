4e56e056ab9e09318bc9dee76e68a2b7f8b821c8
misterhaan <misterhaan@users.noreply.github.com>
2011-03-27 13:49:12 +0000

    rearrange sections

--- feeds/entries.rss.php
+++ feeds/entries.rss.php
@@ -3,20 +3,20 @@
 
   if($_GET['tags'])
     if(substr($_GET['tags'], 0, 1) == '-') {
-      $rss = new auFeed('bln entries', '/output/pen/bln/', 'entries posted on bln at track7 not tagged with ' . substr($_GET['tags'], 1), 'copyright 2008 - 2010 track7');
+      $rss = new auFeed('bln entries', '/bln/', 'entries posted on bln at track7 not tagged with ' . substr($_GET['tags'], 1), 'copyright 2008 - 2011 track7');
       $tags = explode(',', substr($_GET['tags'], 1));
       foreach($tags as $tag)
         $entries .= 'tags=\'' . $tag . '\' or tags like \'%,' . $tag . '\' or tags like \'' . $tag . ',%\' or tags like \'%,' . $tag . ',%\'';
       $entries = 'select name, instant, tags, title, post from bln where status=\'published\' and not (' . $entries . ') order by instant desc';
     } else {
-      $rss = new auFeed('bln entries', '/output/pen/bln/', 'entries posted on bln at track7 tagged with ' . $_GET['tags'], 'copyright 2008 - 2010 track7');
+      $rss = new auFeed('bln entries', '/bln/', 'entries posted on bln at track7 tagged with ' . $_GET['tags'], 'copyright 2008 - 2011 track7');
       $tags = explode(',', $_GET['tags']);
       foreach($tags as $tag)
         $entries .= 'tags=\'' . $tag . '\' or tags like \'%,' . $tag . '\' or tags like \'' . $tag . ',%\' or tags like \'%,' . $tag . ',%\'';
       $entries = 'select name, instant, tags, title, post from bln where status=\'published\' and (' . $entries . ') order by instant desc';
     }
   else {
-    $rss = new auFeed('all bln entries', '/output/pen/bln/', 'all entries posted on bln at track7', 'copyright 2008 - 2010 track7');
+    $rss = new auFeed('all bln entries', '/bln/', 'all entries posted on bln at track7', 'copyright 2008 - 2011 track7');
     $entries = 'select name, instant, tags, title, post from bln where status=\'published\' order by instant desc';
   }
   if($entries = $db->GetLimit($entries, 0, 15, '', ''))
@@ -26,7 +26,7 @@
       $entry->post = str_replace('href="/', 'href="http://' . $_SERVER['HTTP_HOST'] . '/', $entry->post);
       $entry->title = str_replace(array('&lsquo;', '&rsquo;', '&ldquo;', '&rdquo;', '&mdash;'), array('\'', '\'', '"', '"', '--'), $entry->title);
       $tags = '<p>tags:&nbsp; ' . str_replace(',', ', ', $entry->tags) . '</p>';
-      $rss->AddItem($tags . $entry->post . '<p>» <a href="/output/pen/bln/' . $entry->id . '">read more...</a></p>', $entry->title, '/output/pen/bln/' . $entry->name, $entry->instant, '/output/pen/bln/' . $entry->name, true);
+      $rss->AddItem($tags . $entry->post . '<p>» <a href="/bln/' . $entry->id . '">read more...</a></p>', $entry->title, '/bln/' . $entry->name, $entry->instant, '/bln/' . $entry->name, true);
     }
   $rss->End();
 ?>
--- feeds/guides.rss.php
+++ feeds/guides.rss.php
@@ -1,29 +1,28 @@
 <?
   require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/lib/track7.php';
-  require_once 'auFeed.php';
 
   if($_GET['tags'])
     if(substr($_GET['tags'], 0, 1) == '-') {
-      $rss = new auFeed('guides', '/geek/guides/', 'guides posted on track7 not tagged with ' . substr($_GET['tags'], 1), 'copyright 2008 - 2010 track7');
+      $rss = new auFeed('guides', '/guides/', 'guides posted on track7 not tagged with ' . substr($_GET['tags'], 1), 'copyright 2008 - 2011 track7');
       $tags = explode(',', substr($_GET['tags'], 1));
       foreach($tags as $tag)
         $guides .= ' tags=\'' . $tag . '\' or tags like \'%,' . $tag . '\' or tags like \'' . $tag . ',%\' or tags like \'%,' . $tag . ',%\'';
       $guides = 'select id, dateadded, title, description from guides where status=\'approved\' and not (' . $guides . ') order by dateadded desc';
     } else {
-      $rss = new auFeed('guides', '/geek/guides/', 'guides posted on track7 tagged with ' . $_GET['tags'], 'copyright 2008 - 2010 track7');
+      $rss = new auFeed('guides', '/guides/', 'guides posted on track7 tagged with ' . $_GET['tags'], 'copyright 2008 - 2011 track7');
       $tags = explode(',', $_GET['tags']);
       foreach($tags as $tag)
         $guides .= ' tags=\'' . $tag . '\' or tags like \'%,' . $tag . '\' or tags like \'' . $tag . ',%\' or tags like \'%,' . $tag . ',%\'';
       $guides = 'select id, dateadded, title, description from guides where status=\'approved\' and (' . $guides . ') order by dateadded desc';
     }
   else {
-    $rss = new auFeed('guides', '/geek/guides/', 'all guides posted at track7', 'copyright 2008 - 2010 track7');
+    $rss = new auFeed('guides', '/guides/', 'all guides posted at track7', 'copyright 2008 - 2011 track7');
     $guides = 'select id, dateadded, title, description from guides order by dateadded desc';
   }
   if($guides = $db->GetLimit($guides, 0, 15, '', ''))
     while($guide = $guides->NextRecord()) {
       $guide->title = str_replace(array('&lsquo;', '&rsquo;', '&ldquo;', '&rdquo;', '&mdash;'), array('\'', '\'', '"', '"', '--'), $guide->title);
-      $rss->AddItem('<p>' . $guide->description . '</p>', $guide->title, '/geek/guides/' . $guide->id . '/', $guide->dateadded, '/geek/guides/' . $guide->id . '/', true);
+      $rss->AddItem('<p>' . $guide->description . '</p>', $guide->title, '/guides/' . $guide->id . '/', $guide->dateadded, '/guides/' . $guide->id . '/', true);
     }
   $rss->End();
 ?>
