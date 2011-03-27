c259882ba54172f79d386eba8e6849a6b2875021
misterhaan <misterhaan@4369b48c-882d-0410-aabb-eb458be0e6f5>
2011-03-27 13:49:49 +0000

    rearrange sections

---
+++ live/hits/graph-days.png.php
@@ -0,0 +1,102 @@
+<?
+/*----------------------------------------------------------------------------*\
+ | creates an image which is a chart of daily hits.  used by the monthly      |
+ | stats page.                                                                |
+ |                                                                            |
+ | 2008.11.14:                                                                |
+ |  - don't put days with 0 hits on the graph                                 |
+ | 2008.11.11:                                                                |
+ |  - fixed maxy to include oldest day                                        |
+ | 2007.01.23:                                                                |
+ |  - changed to new layout classes                                           |
+ | 2006.04.21:                                                                |
+ |  - changed to new hits tables                                              |
+ |  - increased default width to 450 (from 400)                               |
+ | 2005.06.23:                                                                |
+ |  - changed to new layout classes                                           |
+ |  - increased default width to 400 (from 275)                               |
+ |  - don't do transparent bars, set bar color from style in querystring      |
+ | 2004.06.25:                                                                |
+ |  - added comments                                                          |
+ |                                                                            |
+\*----------------------------------------------------------------------------*/
+
+  require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/lib/track7.php';
+
+  // default width is 450 pixels
+  if(!isset($_GET['w']))
+    $_GET['w'] = 450;
+  // default height is 250 pixels
+  if(!isset($_GET['h']))
+    $_GET['h'] = 250;
+  // default / max number of days is the number of available pixels
+  if(!isset($_GET['d']) || $_GET['d'] + 2 > $_GET['w'])
+    $_GET['d'] = $_GET['w'] - 2;
+
+  $png = imageCreate($_GET['w'], $_GET['h']);
+
+  $white = imageColorAllocate($png, 255, 255, 255);
+  $grey = imageColorAllocate($png, 102, 102, 102);
+  $greybars = imageColorAllocate($png, 204, 204, 204);
+  $bars = imageColorAllocate($png, 153, 102, 85);
+
+  // draw the main graph area
+  imageFilledRectangle($png, 0, 0, $_GET['w'] - 1, $_GET['h'] - 1, $white);
+  imageRectangle($png, 0, 0, $_GET['w'] - 1, $_GET['h'] - 16, $grey);
+
+  if(isset($_GET['month'])) {
+    $where = date('Y-m', strtotime($_GET['month'] . '-01') + 2764800);  // 2764800 seconds == 32 days
+    $where = ' where `date`<\'' . $where . '-31\'';
+  }
+  $stats = 'select `date` from hitcounts' . $where . ' and date like \'%-%-%\' order by `date` desc';
+  $stats = $db->GetLimit($stats, 0, $_GET['d']);
+  if($stats->IsError())
+    imageString($png, 2, 3, 3, 'error reading day statistics', $grey);
+  else {
+    $days = $stats->NumRecords();
+
+    // write the first and last day on the graph
+    $day = $stats->NextRecord();
+    imageString($png, 2, $_GET['w'] - 60, $_GET['h'] - 15, $day->date, $grey);
+    while($nextday = $stats->NextRecord())
+      $day = $nextday;
+    imageString($png, 2, 1, $_GET['h'] - 15, $day->date, $grey);
+
+    // get highest number of hits
+    $maxy = 'select max(`unique`) as maxhits from hitcounts where date like \'%-%-%\' and `date`>=\'' . $day->date . '\'';
+    $maxy = $db->Get($maxy);
+    if($maxy->IsError())
+      imageString($png, 2, 3, 3, 'error trying to find max number of hits', $grey);
+    else {
+      $maxy = $maxy->NextRecord();
+      $maxy = $maxy->maxhits;
+
+      $h = $_GET['h'] - 17;
+      $w = ($_GET['w'] - 2) / $days;
+      $l = $_GET['w'] - 1 - $w;
+
+      $stats = 'select `date`, `unique` as uhits from hitcounts' . $where . ' and date like \'%-%-%\' order by `date` desc';
+      $stats = $db->GetLimit($stats, 0, $_GET['d']);
+      if($stats->IsError())
+        imageString($png, 2, 3, 3, 'error reading day statistics', $grey);
+      else {
+        while($day = $stats->NextRecord()) {
+          if($day->uhits > 0)
+            imageFilledRectangle($png, $l, 1 + $h * (1 - $day->uhits / $maxy), $l + $w - 1, $h, substr($day->date, 0, 7) == $_GET['month'] ? $bars : $greybars);
+          $l -= $w;
+        }
+      }
+    }
+  }
+
+  $lastmod = strtotime(date('Y-m-d') . ' 03:30:00');
+  if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && $lastmod <= strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
+    header('HTTP/1.0 304 Not Modified');
+    die;
+  }
+  header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $lastmod) . ' GMT');
+  header('Content-type: image/png');
+  header('Cache-Control: public');  // allow caching
+  imageInterlace($png,1);
+  imagePNG($png);
+?>
---
+++ live/hits/index.php
@@ -0,0 +1,115 @@
+<?
+  require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/lib/track7.php';
+  $page->start('track7 hit statistics');
+
+  // last 7 days
+  $daily = 'select date, raw, `unique` from hitcounts where date like \'%-%-%\' order by date desc';
+  if($daily = $db->GetLimit($daily, 0, 7, 'error reading daily statistics', '')) {
+    $page->Heading('daily statistics');
+?>
+      <img class="hitchart" src="graph-days.png?d=93&amp;month=<?=date('Y-m'); ?>" alt="" />
+      <table class="data" id="datestats" cellspacing="0">
+        <thead><tr><th>date</th><th title="total hits">raw</th><th title="total hits excluding hits from the same ip within 6 hours">unique</th><th title="average number of pages viewed per visit (raw / unique)">pages</th></tr></thead>
+        <tbody>
+<?
+    if($user->GodMode) {
+      while($day = $daily->NextRecord()) {
+?>
+          <tr><td class="date"><a href="detail.php?day=<?=$day->date; ?>"><?=date('m·d·Y', strtotime($day->date)); ?></a></td><td class="number"><?=$day->raw; ?></td><td class="number"><?=$day->unique; ?></td><td class="number"><?=$day->unique > 0 ? number_format($day->raw / $day->unique, 1) : ''; ?></td><td class="clear"><a href="hits.php?date=<?=$day->date; ?>"><img src="/style/details.png" alt="details" /></a></td></tr>
+<?
+      }
+    } else {
+      while($day = $daily->NextRecord()) {
+?>
+          <tr><td class="date"><a href="detail.php?day=<?=$day->date; ?>"><?=date('m·d·Y', strtotime($day->date)); ?></a></td><td class="number"><?=$day->raw; ?></td><td class="number"><?=$day->unique; ?></td><td class="number"><?=$day->unique > 0 ? number_format($day->raw / $day->unique, 1) : ''; ?></td></tr>
+<?
+      }
+    }
+?>
+        </tbody>
+      </table>
+
+<?
+  }
+
+  // last 4 weeks
+  $weekly = 'select date, raw/days as raw, `unique`/days as `unique` from hitcounts where date like \'%w%\' and days>0 order by date desc';
+  if($weekly = $db->GetLimit($weekly, 0, 4, 'error reading weekly statistics', '')) {
+    $page->Heading('weekly statistics');
+?>
+      <img class="hitchart" src="graph-weeks.png?k=78&amp;year=<?=date('Y'); ?>" alt="" />
+      <table class="data" id="datestats" cellspacing="0">
+        <thead><tr><th>week</th><th title="average hits per day">raw</th><th title="average hits per day excluding hits from the same ip within 6 hours">unique</th><th title="average number of pages viewed per visit (raw / unique)">pages</th></tr></thead>
+        <tbody>
+<?
+    while($week = $weekly->NextRecord()) {
+?>
+          <tr><td class="date"><a href="detail.php?week=<?=$week->date; ?>">week <?=+substr($week->date, 5); ?></a></td><td class="number"><?=number_format($week->raw, 1); ?></td><td class="number"><?=number_format($week->unique, 1); ?></td><td class="number"><?=$week->unique > 0 ? number_format($week->raw / $week->unique, 1) : ''; ?></td></tr>
+<?
+    }
+?>
+        </tbody>
+      </table>
+
+<?
+  }
+
+  // last 6 months
+  $monthly = 'select date, raw/days as raw, `unique`/days as `unique` from hitcounts where date like \'%-%\' and not (date like \'%-%-%\') and days>0 order by date desc';
+  if($monthly = $db->GetLimit($monthly, 0, 6, 'error reading monthly statistics', '')) {
+    $page->Heading('monthly statistics');
+?>
+      <img class="hitchart" src="graph-months.png?year=<?=date('Y'); ?>" alt="" />
+      <table class="data" id="datestats" cellspacing="0">
+        <thead><tr><th>month</th><th title="average hits per day">raw</th><th title="average hits per day excluding hits from the same ip within 6 hours">unique</th><th title="average number of pages viewed per visit (raw / unique)">pages</th></tr></thead>
+        <tbody>
+<?
+    while($month = $monthly->NextRecord()) {
+?>
+          <tr><td class="date"><a href="detail.php?month=<?=$month->date; ?>"><?=strtolower(date('M Y', strtotime($month->date . '-15'))); ?></a></td><td class="number"><?=number_format($month->raw, 1); ?></td><td class="number"><?=number_format($month->unique, 1); ?></td><td class="number"><?=$month->unique > 0 ? number_format($month->raw / $month->unique, 1) : ''; ?></td></tr>
+<?
+    }
+?>
+        </tbody>
+      </table>
+
+<?
+  }
+
+  // all years
+  $yearly = 'select date, raw/days as raw, `unique`/days as `unique` from hitcounts where not(date like \'%-%\' or date like \'%w%\') and days>0 order by date desc';
+  if($yearly = $db->Get($yearly, 'error reading yearly statistics', '')) {
+    $page->Heading('yearly statistics');
+?>
+      <img class="hitchart" src="graph-years.png" alt="" />
+      <table class="data" id="datestats" cellspacing="0">
+        <thead><tr><th>year</th><th title="average hits per day">raw</th><th title="average hits per day excluding hits from the same ip within 6 hours">unique</th><th title="average number of pages viewed per visit (raw / unique)">pages</th></tr></thead>
+        <tbody>
+<?
+    while($year = $yearly->NextRecord()) {
+?>
+          <tr><td class="date"><a href="detail.php?year=<?=$year->date; ?>"><?=$year->date; ?></a></td><td class="number"><?=number_format($year->raw, 1); ?></td><td class="number"><?=number_format($year->unique, 1); ?></td><td class="number"><?=$year->unique > 0 ? number_format($year->raw / $year->unique, 1) : ''; ?></td></tr>
+<?
+    }
+?>
+        </tbody>
+      </table>
+
+<?
+  }
+
+  // forever, from totalling all years
+  $forever = 'select sum(raw)/sum(days) as raw, sum(`unique`)/sum(days) as `unique` from hitcounts where not (date like \'%-%\' or date like \'%w%\')';
+  if($forever = $db->GetRecord($forever, 'error reading forever statistics', '')) {
+    $page->Heading('overall');
+?>
+      <table class="data" id="datestats" cellspacing="0">
+        <thead><tr><th></th><th title="average hits per day">raw</th><th title="average hits per day excluding hits from the same ip within 6 hours">unique</th><th title="average number of pages viewed per visit (raw / unique)">pages</th></tr></thead>
+        <tbody><tr><td class="date"><a href="detail.php">overall</a></td><td class="number"><?=number_format($forever->raw, 1); ?></td><td class="number"><?=number_format($forever->unique, 1); ?></td><td class="number"><?=$forever->unique > 0 ? number_format($forever->raw / $forever->unique, 1) : ''; ?></td></tr></tbody>
+      </table>
+
+<?
+  }
+
+  $page->End();
+?>
