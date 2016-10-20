3080c7fddf9b419eb68ced1141907964b2f656d3
misterhaan <misterhaan@users.noreply.github.com>
2016-10-20 09:37:25 -0500

    stop registration for old user system

--- user/register.php
+++ user/register.php
@@ -27,12 +27,17 @@
     $page->Info('you are currently logged in as ' . $user->Name);
 ?>
       <p>
-        register for an account at track7 to enjoy certain <a href="/user/">benefits</a>
+        the <a href="/bln/redesign-2016">new design</a> brings with it a new
+        login system!  registering for an account with the old login system
+        won’t do much good, so you can’t do that anymore.  visit any page using
+        the new design (for example, the <a href="/">front page</a>) where the
+        sign in link in the upper right will either sign you in with an existing
+        account or sign up with a new account.
       </p>
 
 <?
-  unset($_POST['pass1'], $_POST['pass2']);  // clear the passwords so they don't get written out in the form
-  $reg->WriteHTML();
+  //unset($_POST['pass1'], $_POST['pass2']);  // clear the passwords so they don't get written out in the form
+  //$reg->WriteHTML();
 
   $page->End();
 ?>
