4d53784b01444c8ffb2b519f0fc7c9de6584366a
misterhaan <misterhaan@users.noreply.github.com>
2016-11-10 15:08:06 -0600

    new layout for stories

--- pen/index.php
+++ pen/index.php
@@ -1,54 +1,53 @@
-<?
-  require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/lib/track7.php';
+<?php
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/class/t7.php';
 
-  $jump[] = 'bln';
-  $sections = 'select id, name, description from pensections order by sort';
-  if($sections = $db->Get($sections, 'Error looking up sections', 'No sections defined')) {
-    while($section = $sections->NextRecord()) {
-      $sects[] = $section;
-      $jump[] = $section->id;
+  if(isset($_GET['ajax'])) {
+    $ajax = new t7ajax();
+    switch($_GET['ajax']) {
+      case 'stories':
+        if($stories = $db->query('select ifnull(ss.lastposted,s.posted) as posted, if(ss.url is null,s.url,concat(ss.url,\'/\')) as url, ifnull(ss.title,s.title) as title, ifnull(ss.deschtml,s.deschtml) as deschtml, ss.numstories from stories as s left join stories_series as ss on ss.id=s.series where s.published=1 group by ifnull(s.series,-s.id) order by posted desc')) {
+          $ajax->Data->stories = [];
+          while($story = $stories->fetch_object()) {
+            if($story->posted > 100)
+              $story->posted = t7format::TimeTag('M j, Y', $story->posted, 'g:i a \o\n l F jS Y');
+            else
+              $story->posted = false;
+            $ajax->Data->stories[] = $story;
+          }
+        } else
+          $ajax->Fail('database error looking up stories.');
+        break;
     }
-    $page->Start('pen vs. sword', null, '<em>which will prove mightier?</em>', '', $jump);
-    $page->Heading('bln', 'bln');
+    $ajax->Send();
+    die;
+  }
+
+  $html = new t7html(['ko' => true]);
+  $html->Open('stories');
 ?>
+      <h1>stories</h1>
+
       <p>
-        bln is a &lsquo;natural blog&rsquo; because i thought that was a clever
-        name.
+        one of the things i did to pass the time at school was write stories.
+        it actually started in a junior high english class where the teacher was
+        trying to show us that writing was fun, and i stuck with it until my
+        college classes needed more of my focus.  some of the stories here i
+        handed in as assignments, including some that are actually essays and
+        not stories at all.  one of them is even a poem.  my more recent writing
+        has been in the <a href="/bln/">blog</a> but i have a few ideas i might
+        try to get out as stories.
       </p>
-      <dl>
-        <dt><a href="/bln/">a natural blog</a></dt>
-        <dd>
 
-<?
-    $page->TagCloud('entries', '/bln/tag=', 2, 4, 8, 16);  // keep in sync with bln/index.php
-?>
-        </dd>
-      </dl>
-
-<?
-
-    foreach($sects as $sect) {
-      $page->Heading($sect->name, $sect->id);
-      echo $sect->description;
-      $stories = 'select id, name, description, posted from penstories where section=\'' . $sect->id . '\' order by sort';
-      if($stories = $db->Get($stories, 'Error looking up stories', '')) {
-?>
-      <dl>
-<?
-        while($story = $stories->NextRecord()) {
-          echo '        <dt>';
-          if($story->posted)
-            echo '<span class="when">' . $story->posted . '</span>';
-          echo '<a href="' . $sect->id . '/' . $story->id . '.php">' . $story->name . "</a></dt>\n";
-          if($story->description)
-            echo '        <dd>' . $story->description . "</dd>\n";
-        }
-?>
-      </dl>
-<?
-      }
-    }
-  } else
-    $page->Start('pen vs. sword', null, '<em>which will prove mightier?</em>');
-  $page->End();
+      <!-- ko foreach: stories -->
+      <article data-bind="css: {series: numstories}">
+        <h2><a data-bind="text: title, attr: {href: url}"></a></h2>
+        <p class=postmeta data-bind="visible: posted || numstories">
+          <span data-bind="visible: numstories, text: 'a series of ' + numstories + ' stories'"></span>
+          <span data-bind="visible: posted">posted <time data-bind="text: posted.display, attr: {datetime: posted.datetime, title: posted.title}"></time></span>
+        </p>
+        <div class=description data-bind="html: deschtml"></div>
+      </article>
+      <!-- /ko -->
+<?php
+  $html->Close();
 ?>
--- pen/story.php
+++ pen/story.php
@@ -1,20 +1,61 @@
-<?
-  require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/lib/track7.php';
-
-  if(isset($_GET['id'])) {
-    $story = 'select id, name, title, subtitle, pretitle, section from penstories where id=\'' . addslashes($_GET['id']) . '\'';
-    if($story = $db->GetRecord($story, '', '')) {
-      $url = dirname($_SERVER['PHP_SELF']) . '/' . $story->section . '/' . $story->id . '.php';
-      if($_SERVER['REQUEST_URI'] != $url) {
-        header('Location: http://' . $_SERVER['HTTP_HOST'] . $url);
-        die;
-      }
-      $page->Start($story->name . ' - ' . $story->section . ' - pen vs. sword - output', $story->title, $story->subtitle, $story->pretitle);
-      include _ROOT . dirname($_SERVER['PHP_SELF']) . '/' . $story->id . '.html';
-      $page->SetFlag(_FLAG_PAGES_COMMENTS);
-      $page->End();
-      die;
-    }
+<?php
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/class/t7.php';
+  if($story = $db->query('select s.id, s.published, s.url, ss.url as seriesurl, ss.title as seriestitle, ss.numstories, s.number, s.title, s.storyhtml from stories as s left join stories_series as ss on ss.id=s.series where s.url=\'' . $db->escape_string($_GET['story']) . '\''))
+    if($story = $story->fetch_object())
+      if($story->published || $user->IsAdmin()) {
+        if($story->seriesurl && (!isset($_GET['series']) || $_GET['series'] != $story->seriesurl)) {
+          header('Location: http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/' . $story->seriesurl . '/' . $story->url);
+          die;
+        }
+        if(!$story->seriesurl && isset($_GET['series'])) {
+          header('Location: http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/' . $story->url);
+          die;
+        }
+        $html = new t7html(['ko' => true]);
+        if($story->seriesurl) {
+          $html->Open(htmlspecialchars($story->title) . ' - ' . htmlspecialchars($story->seriestitle) . ' - stories');
+?>
+      <h1><?php echo htmlspecialchars($story->title); ?></h1>
+      <p class=postmeta>
+        story <?php echo +$story->number; ?> of <?php echo +$story->numstories; ?>
+        in <a href="."><?php echo $story->seriestitle; ?></a>
+      </p>
+<?php
+        } else {
+          $html->Open(htmlspecialchars($story->title) . ' - stories');
+?>
+      <h1><?php echo htmlspecialchars($story->title); ?></h1>
+<?php
+        }
+        echo $story->storyhtml;
+        // TODO:  links to prev / next story and prev / next in series
+        $html->ShowComments('story', 'stories', $story->id);
+        $html->Close();
+      } else
+        StoryNotFound();
+    else
+      StoryNotFound();
+  else {
+    $html = new t7html([]);
+    $html->Open('database error - stories');
+?>
+      <h1>database error</h1>
+      <p class=error>a database error got in the way of showing you this story.</p>
+<?php
+    $html->Close();
   }
-  $page->Show404();
+
+  function StoryNotFound() {
+    header('HTTP/1.0 404 Not Found');
+    $html = new t7html([]);
+    $html->Open('story not found');
+?>
+      <h1>404 story not found</h1>
+      <p>
+        sorry, we don’t seem to have a story by that name.  try the list of
+        <a href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>/">all stories</a>.
+      </p>
+<?php
+    $html->Close();
+  }
 ?>
