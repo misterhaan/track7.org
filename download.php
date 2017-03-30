a74b855564fc0b68fb87b94f95cb1987ad921cd3
misterhaan <misterhaan@users.noreply.github.com>
2017-03-30 11:23:17 -0500

    clean up unused stuff

--- download.php
+++
@@ -1,8 +0,0 @@
-<?
-  if(isset($_GET['file']) && file_exists($_SERVER['DOCUMENT_ROOT'] . '/' . $_GET['file'])) {
-    require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/lib/track7.php';
-    $page->LogHit('/' . $_GET['file']);
-    header('Location: http://' . $_SERVER['HTTP_HOST'] . '/' . $_GET['file']);
-    die;
-  }
-?>
--- etc/svn-hooks/post-commit.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Updating live site..."
-cd /home/misterhaan/track7.org
-/usr/bin/svn update
