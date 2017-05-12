ea30a4a19d195d2e47afd61358091f13b8a86651
misterhaan <misterhaan@users.noreply.github.com>
2017-05-12 16:22:41 -0500

    spaces to tabs

--- code/vs/application.php
+++ code/vs/application.php
@@ -1,124 +1,123 @@
 <?php
-  require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/class/t7.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/class/t7.php';
 
-  $app = false;
-  if(isset($_GET['url']) && $app = $db->query('select id, url, name, deschtml, github, wiki from code_vs_applications where url=\'' . $db->escape_string($_GET['url']) . '\' limit 1'))
-    $app = $app->fetch_object();
-  if(!$app) { // TODO:  make sure there’s at least one release   || $entry->status != 'published' && !$user->IsAdmin()) {
-    header('HTTP/1.0 404 Not Found');
-    $html = new t7html([]);
-    $html->Open('application not found - software');
+$app = false;
+if(isset($_GET['url']) && $app = $db->query('select id, url, name, deschtml, github, wiki from code_vs_applications where url=\'' . $db->escape_string($_GET['url']) . '\' limit 1'))
+	$app = $app->fetch_object();
+if(!$app) { // TODO:  make sure there’s at least one release   || $entry->status != 'published' && !$user->IsAdmin()) {
+	header('HTTP/1.0 404 Not Found');
+	$html = new t7html([]);
+	$html->Open('application not found - software');
 ?>
-      <h1>404 application not found</h1>
+			<h1>404 application not found</h1>
 
-      <p>
-        sorry, we don’t seem to have an application by that name.  try the list of
-        <a href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>/">all applications</a>.
-      </p>
+			<p>
+				sorry, we don’t seem to have an application by that name.  try the list of
+				<a href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>/">all applications</a>.
+			</p>
 <?php
-    $html->Close();
-    die;
-  }
+	$html->Close();
+	die;
+}
 
-  $html = new t7html(['ko' => true]);
-  $html->Open(htmlspecialchars($app->name));
+$html = new t7html(['ko' => true]);
+$html->Open(htmlspecialchars($app->name));
 ?>
-      <h1>
-        <img class=icon src="files/<?php echo $app->url; ?>.png" alt="">
-        <?php echo htmlspecialchars($app->name); ?>
-      </h1>
+			<h1>
+				<img class=icon src="files/<?php echo $app->url; ?>.png" alt="">
+				<?php echo htmlspecialchars($app->name); ?>
+			</h1>
 <?php
-  if($user->IsAdmin()) {
+if($user->IsAdmin()) {
 ?>
-      <nav class=actions>
-        <a class=edit href="editapp?id=<?php echo $app->id; ?>">edit this application</a>
-        <a class=new href="addrel?app=<?php echo $app->id; ?>">add a release</a>
-      </nav>
+			<nav class=actions>
+				<a class=edit href="editapp?id=<?php echo $app->id; ?>">edit this application</a>
+				<a class=new href="addrel?app=<?php echo $app->id; ?>">add a release</a>
+			</nav>
 <?php
-  }
-  echo $app->deschtml;
-  if($app->github) {
+}
+echo $app->deschtml;
+if($app->github) {
 ?>
-      <p class=calltoaction><a class="action github" href="https://github.com/misterhaan/<?php echo $app->github; ?>"><?php echo htmlspecialchars($app->name); ?> on github</a></p>
+			<p class=calltoaction><a class="action github" href="https://github.com/misterhaan/<?php echo $app->github; ?>"><?php echo htmlspecialchars($app->name); ?> on github</a></p>
 <?php
-  }
-  if($app->wiki) {
+}
+if($app->wiki) {
 ?>
-      <p class=calltoaction><a class="action documentation" href="http://wiki.track7.org/<?php echo $app->wiki; ?>"><?php echo htmlspecialchars($app->name); ?> documentation</a></p>
+			<p class=calltoaction><a class="action documentation" href="http://wiki.track7.org/<?php echo $app->wiki; ?>"><?php echo htmlspecialchars($app->name); ?> documentation</a></p>
 <?php
-  }
-  if($rels = $db->query('select r.released, concat(r.major, \'.\', r.minor, \'.\', r.revision) as version, r.binurl, r.bin32url, r.srcurl, r.changelog, r.lang, r.dotnet, r.studio from (select r.application, r.released, r.major, r.minor, r.revision, r.binurl, r.bin32url, r.srcurl, r.changelog, l.abbr as lang, if(n.version is not null, concat(\'.net \', n.version), \'\') as dotnet, s.name as studio from code_vs_releases as r left join code_vs_lang as l on l.id=r.lang left join code_vs_dotnet as n on n.id=r.dotnet left join code_vs_studio as s on s.version=r.studio where r.application=\'' . +$app->id . '\' order by major desc, minor desc, revision desc) as r group by major, minor order by r.released desc'))
-    while($rel = $rels->fetch_object()) {
-      $released = t7format::TimeTag('smart', $rel->released, 'Y-m-d g:i:s a');
-      if(strpos($rel->binurl, '/') === false)
-      	$rel->binurl = 'files/' . $rel->binurl;
-      if($rel->bin32url && strpos($rel->bin32url, '/') === false)
-      	$rel->bin32url = 'files/' . $rel->bin32url;
+}
+if($rels = $db->query('select r.released, concat(r.major, \'.\', r.minor, \'.\', r.revision) as version, r.binurl, r.bin32url, r.srcurl, r.changelog, r.lang, r.dotnet, r.studio from (select r.application, r.released, r.major, r.minor, r.revision, r.binurl, r.bin32url, r.srcurl, r.changelog, l.abbr as lang, if(n.version is not null, concat(\'.net \', n.version), \'\') as dotnet, s.name as studio from code_vs_releases as r left join code_vs_lang as l on l.id=r.lang left join code_vs_dotnet as n on n.id=r.dotnet left join code_vs_studio as s on s.version=r.studio where r.application=\'' . +$app->id . '\' order by major desc, minor desc, revision desc) as r group by major, minor order by r.released desc'))
+	while($rel = $rels->fetch_object()) {
+		$released = t7format::TimeTag('smart', $rel->released, 'Y-m-d g:i:s a');
+		if(strpos($rel->binurl, '/') === false)
+			$rel->binurl = 'files/' . $rel->binurl;
+		if($rel->bin32url && strpos($rel->bin32url, '/') === false)
+			$rel->bin32url = 'files/' . $rel->bin32url;
 ?>
-      <article>
-        <header>
-          <h2>version <?php echo $rel->version; ?></h2>
-          <p class=guidemeta>
-            <time class=posted datetime="<?php echo $released->datetime; ?>" title="released <?php echo $released->title; ?>"><?php echo $released->display; ?></time>
+			<article>
+				<header>
+					<h2>version <?php echo $rel->version; ?></h2>
+					<p class=guidemeta>
+						<time class=posted datetime="<?php echo $released->datetime; ?>" title="released <?php echo $released->title; ?>"><?php echo $released->display; ?></time>
 <?php
-      if($rel->dotnet) {
+		if($rel->dotnet) {
 ?>
-            <span class=dotnet><?php echo $rel->dotnet; ?></span>
+						<span class=dotnet><?php echo $rel->dotnet; ?></span>
 <?php
-      }
+		}
 ?>
-            <span class=lang><?php echo $rel->lang; ?></span>
-            <span class=studio><?php echo $rel->studio; ?></span>
-          </p>
-        </header>
-        <?php if($rel->changelog) echo $rel->changelog; ?>
-        <nav class="calltoaction downloads">
-          <a class="action <?php echo GetExtension($rel->binurl); ?>" href="<?php echo $rel->binurl; ?>"><?php echo GetName($rel->binurl, $rel->version, $rel->bin32url ? 64 : 0); ?></a>
+						<span class=lang><?php echo $rel->lang; ?></span>
+						<span class=studio><?php echo $rel->studio; ?></span>
+					</p>
+				</header>
+				<?php if($rel->changelog) echo $rel->changelog; ?>
+				<nav class="calltoaction downloads">
+					<a class="action <?php echo GetExtension($rel->binurl); ?>" href="<?php echo $rel->binurl; ?>"><?php echo GetName($rel->binurl, $rel->version, $rel->bin32url ? 64 : 0); ?></a>
 <?php
-      if($rel->bin32url) {
+		if($rel->bin32url) {
 ?>
-          <a class="action <?php echo GetExtension($rel->bin32url); ?>" href="<?php echo $rel->bin32url; ?>"><?php echo GetName($rel->bin32url, $rel->version, 32); ?></a>
+					<a class="action <?php echo GetExtension($rel->bin32url); ?>" href="<?php echo $rel->bin32url; ?>"><?php echo GetName($rel->bin32url, $rel->version, 32); ?></a>
 <?php
-      }
-      if($rel->srcurl) {
+		}
+		if($rel->srcurl) {
 ?>
-          <a class="action <?php echo GetCodeType($rel->srcurl); ?>" href="<?php echo $rel->srcurl; ?>">source v<?php echo $rel->version; ?></a>
+					<a class="action <?php echo GetCodeType($rel->srcurl); ?>" href="<?php echo $rel->srcurl; ?>">source v<?php echo $rel->version; ?></a>
 <?php
-      }
+		}
 ?>
-        </nav>
-      </article>
+				</nav>
+			</article>
 <?php
-    }
-  $html->ShowComments('application', 'code_vs', $app->id);
-  $html->Close();
+	}
+$html->ShowComments('application', 'code_vs', $app->id);
+$html->Close();
 
-  function GetName($url, $version, $bits = 0) {
-    $name = '';
-    switch(GetExtension($url)) {
-      case 'msi':
-        $name = 'installer';
-        break;
-      case 'zip':
-        $name = 'binaries';
-        break;
-    }
-    $name .= ' v' . $version;
-    if($bits)
-      $name .= ' (' . $bits . '-bit)';
-    return $name;
-  }
+function GetName($url, $version, $bits = 0) {
+	$name = '';
+	switch(GetExtension($url)) {
+		case 'msi':
+			$name = 'installer';
+			break;
+		case 'zip':
+			$name = 'binaries';
+			break;
+	}
+	$name .= ' v' . $version;
+	if($bits)
+		$name .= ' (' . $bits . '-bit)';
+	return $name;
+}
 
-  function GetExtension($filename) {
-    $parts = explode('.', $filename);
-    return $parts[count($parts) - 1];
-  }
+function GetExtension($filename) {
+	$parts = explode('.', $filename);
+	return $parts[count($parts) - 1];
+}
 
-  function GetCodeType($url) {
-    if(substr($url, 0, 19) == 'https://github.com/')
-      return 'github';
-    if(substr($url, 0, 8) == 'https://' || substr($url, 0, 7) == 'http://')
-      return 'link';
-    return 'zip';
-  }
-?>
+function GetCodeType($url) {
+	if(substr($url, 0, 19) == 'https://github.com/')
+		return 'github';
+	if(substr($url, 0, 8) == 'https://' || substr($url, 0, 7) == 'http://')
+		return 'link';
+	return 'zip';
+}
--- code/vs/index.php
+++ code/vs/index.php
@@ -1,63 +1,62 @@
 <?php
-  require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/class/t7.php';
-  $html = new t7html([]);
-  $html->Open('applications');
+require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/class/t7.php';
+$html = new t7html([]);
+$html->Open('applications');
 ?>
-      <h1>
-        applications
-        <a class=feed href="<?php echo dirname($_SERVER['PHP_SELF']); ?>/feed.rss" title="rss feed of application releases"><img alt=feed src="/images/feed.png"></a>
-      </h1>
+			<h1>
+				applications
+				<a class=feed href="<?php echo dirname($_SERVER['PHP_SELF']); ?>/feed.rss" title="rss feed of application releases"><img alt=feed src="/images/feed.png"></a>
+			</h1>
 <?php
-  if($user->IsAdmin()) {
+if($user->IsAdmin()) {
 ?>
-      <nav class=actions><a class=new href="editapp">add an application</a></nav>
+			<nav class=actions><a class=new href="editapp">add an application</a></nav>
 <?php
-  }
+}
 ?>
-      <p>
-        each application is available in a windows installer package, unless
-        it’s older than windows installer in which case it’s a zip file with
-        a setup.exe and a couple other files.
-      </p>
-      <p>
-        source code for each release is also provided so you can customize and /
-        or learn from my work.  newer releases are on github, preceded by
-        subversion, and zip files before that.
-      </p>
+			<p>
+				each application is available in a windows installer package, unless
+				it’s older than windows installer in which case it’s a zip file with
+				a setup.exe and a couple other files.
+			</p>
+			<p>
+				source code for each release is also provided so you can customize and /
+				or learn from my work.  newer releases are on github, preceded by
+				subversion, and zip files before that.
+			</p>
 <?php
-  if($apps = $db->query('select * from (select a.id, a.url, a.name, a.deschtml, r.version, r.released, r.binurl, r.bin32url from code_vs_applications as a join (select * from (select application, concat(major, \'.\', minor, \'.\', revision) as version, released, binurl, bin32url from code_vs_releases order by application, released desc) as rls) as r on r.application=a.id where r.application is not null order by a.id, r.released desc) as ar group by ar.id order by ar.released desc')) {
-    $is64 = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'x86_64';
-    $is64 = stripos($is64, 'x86_64') !== false || stripos($is64, 'x86-64') !== false || stripos($is64, 'win64') !== false || stripos($is64, 'x64;') !== false || stripos($is64, 'amd64') !== false || stripos($is64, 'wow64') !== false || stripos($is64, 'x64_64') !== false;
+if($apps = $db->query('select * from (select a.id, a.url, a.name, a.deschtml, r.version, r.released, r.binurl, r.bin32url from code_vs_applications as a join (select * from (select application, concat(major, \'.\', minor, \'.\', revision) as version, released, binurl, bin32url from code_vs_releases order by application, released desc) as rls) as r on r.application=a.id where r.application is not null order by a.id, r.released desc) as ar group by ar.id order by ar.released desc')) {
+	$is64 = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'x86_64';
+	$is64 = stripos($is64, 'x86_64') !== false || stripos($is64, 'x86-64') !== false || stripos($is64, 'win64') !== false || stripos($is64, 'x64;') !== false || stripos($is64, 'amd64') !== false || stripos($is64, 'wow64') !== false || stripos($is64, 'x64_64') !== false;
 ?>
-      <nav id=vsapps>
+			<nav id=vsapps>
 <?php
-    while($app = $apps->fetch_object()) {
-      $app->released = t7format::TimeTag('smart', $app->released, 'M j, Y \a\t g:i a');
-      if(strpos($app->binurl, '/') === false)
-      	$app->binurl = 'files/' . $app->binurl;
-      if($app->bin32url && strpos($app->bin32url, '/') === false)
-      	$app->bin32url = 'files/' . $app->bin32url;
+	while($app = $apps->fetch_object()) {
+		$app->released = t7format::TimeTag('smart', $app->released, 'M j, Y \a\t g:i a');
+		if(strpos($app->binurl, '/') === false)
+			$app->binurl = 'files/' . $app->binurl;
+		if($app->bin32url && strpos($app->bin32url, '/') === false)
+			$app->bin32url = 'files/' . $app->bin32url;
 ?>
-        <article>
-          <header>
-            <h2><a href="<?php echo $app->url; ?>">
-              <img class=icon src="files/<?php echo $app->url; ?>.png" alt="">
-              <?php echo htmlspecialchars($app->name); ?>
-            </a></h2>
-            <p class=guidemeta>
-              <span class=version title="latest version <?php echo $app->version; ?>">v<?php echo $app->version; ?></span>
-              <time class=posted title="latest release <?php echo $app->released->title; ?>" datetime="<?php echo $app->released->datetime; ?>"><?php echo $app->released->display; ?></time>
-            </p>
-          </header>
-          <?php echo $app->deschtml; ?>
-          <p class="calltoaction downloads"><a class="action download" href="<?php echo $is64 || !$app->bin32url ? $app->binurl : $app->binurl; ?>">download latest release<?php if($app->bin32url) echo ' (' . ($is64 ? '64' : '32') . '-bit)'; ?></a></p>
-          <p class="calltoaction downloads"><a class="action list" href="<?php echo $app->url; ?>">other versions and source code</a></p>
-        </article>
+				<article>
+					<header>
+						<h2><a href="<?php echo $app->url; ?>">
+							<img class=icon src="files/<?php echo $app->url; ?>.png" alt="">
+							<?php echo htmlspecialchars($app->name); ?>
+						</a></h2>
+						<p class=guidemeta>
+							<span class=version title="latest version <?php echo $app->version; ?>">v<?php echo $app->version; ?></span>
+							<time class=posted title="latest release <?php echo $app->released->title; ?>" datetime="<?php echo $app->released->datetime; ?>"><?php echo $app->released->display; ?></time>
+						</p>
+					</header>
+					<?php echo $app->deschtml; ?>
+					<p class="calltoaction downloads"><a class="action download" href="<?php echo $is64 || !$app->bin32url ? $app->binurl : $app->binurl; ?>">download latest release<?php if($app->bin32url) echo ' (' . ($is64 ? '64' : '32') . '-bit)'; ?></a></p>
+					<p class="calltoaction downloads"><a class="action list" href="<?php echo $app->url; ?>">other versions and source code</a></p>
+				</article>
 <?php
-    }
+	}
 ?>
-      </nav>
+			</nav>
 <?php
-  }
-  $html->Close();
-?>
+}
+$html->Close();
