a92319823e4730e9caaf5b8774518e4b3d7f4ae3
misterhaan <misterhaan@users.noreply.github.com>
2017-09-04 15:29:18 -0500

    clear cached files from cloudflare when updated by git (fixes #12)
    (untested)

--- etc/class/t7keys.template.php
+++ etc/class/t7keys.template.php
@@ -1,127 +1,147 @@
 <?php
-  /**
-   * access keys template for t7 classes.  copy one directory level up from
-   * document root, name .t7keys.php, and fill in with access values.  this
-   * obviously needs to be left blank on github to avoid sharing secrets.
-   */
+/**
+ * access keys template for t7 classes.  copy one directory level up from
+ * document root, name .t7keys.php, and fill in with access values.  this
+ * obviously needs to be left blank on github to avoid sharing secrets.
+ */
 
-  class t7keysDB {
-    /**
-     * hostname for database (often this is localhost)
-     * @var string
-     */
-    const HOST = '';
+class t7keysDB {
+	/**
+	 * hostname for database (often this is localhost)
+	 * @var string
+	 */
+	const HOST = '';
 
-    /**
-     * name of database
-     * @var string
-     */
-    const NAME = '';
+	/**
+	 * name of database
+	 * @var string
+	 */
+	const NAME = '';
 
-    /**
-     * username with access to the database
-     * @var string
-     */
-    const USER = '';
+	/**
+	 * username with access to the database
+	 * @var string
+	 */
+	const USER = '';
 
-    /**
-     * password for user with access to the database
-     * @var string
-     */
-    const PASS = '';
-  }
+	/**
+	 * password for user with access to the database
+	 * @var string
+	 */
+	const PASS = '';
+}
 
-  class t7keysGoogle {
-    /**
-     * client id from google developer console
-     * @var string
-     */
-    const ID = '';
+class t7keysGoogle {
+	/**
+	 * client id from google developer console
+	 * @var string
+	 */
+	const ID = '';
 
-    /**
-     * client secret from google developer console
-     * @var string
-     */
-    const SECRET = '';
-  }
+	/**
+	 * client secret from google developer console
+	 * @var string
+	 */
+	const SECRET = '';
+}
 
-  class t7keysTwitter {
-    /**
-     * consumer key (api key) from apps.twitter.com after selecting your app
-     * @var string
-     */
-    const CONSUMER_KEY = '';
+class t7keysTwitter {
+	/**
+	 * consumer key (api key) from apps.twitter.com after selecting your app
+	 * @var string
+	 */
+	const CONSUMER_KEY = '';
 
-    /**
-     * consumer secret (api secret) from apps.twitter.com after selecting your app
-     * @var string
-     */
-    const CONSUMER_SECRET = '';
+	/**
+	 * consumer secret (api secret) from apps.twitter.com after selecting your app
+	 * @var string
+	 */
+	const CONSUMER_SECRET = '';
 
-    /**
-     * access token from apps.twitter.com under your access token heading
-     * @var string
-     */
-    const OAUTH_TOKEN = '';
+	/**
+	 * access token from apps.twitter.com under your access token heading
+	 * @var string
+	 */
+	const OAUTH_TOKEN = '';
 
-    /**
-     * access token secret from apps.twitter.com under your access token heading
-     * @var string
-     */
-    const OAUTH_TOKEN_SECRET = '';
-  }
+	/**
+	 * access token secret from apps.twitter.com under your access token heading
+	 * @var string
+	 */
+	const OAUTH_TOKEN_SECRET = '';
+}
 
-  class t7keysFacebook {
-    /**
-     * app id from facebook app basic settings
-     * @var string
-     */
-    const ID = '';
+class t7keysFacebook {
+	/**
+	 * app id from facebook app basic settings
+	 * @var string
+	 */
+	const ID = '';
 
-    /**
-     * client secret from facebook app basic settings
-     * @var string
-     */
-    const SECRET = '';
-  }
+	/**
+	 * client secret from facebook app basic settings
+	 * @var string
+	 */
+	const SECRET = '';
+}
 
-  class t7keysTweet {
-    /**
-     * consumer key (api key) from apps.twitter.com after selecting your app
-     * @var string
-     */
-    const CONSUMER_KEY = '';
+class t7keysTweet {
+	/**
+	 * consumer key (api key) from apps.twitter.com after selecting your app
+	 * @var string
+	 */
+	const CONSUMER_KEY = '';
 
-    /**
-     * consumer secret (api secret) from apps.twitter.com after selecting your app
-     * @var string
-     */
-    const CONSUMER_SECRET = '';
+	/**
+	 * consumer secret (api secret) from apps.twitter.com after selecting your app
+	 * @var string
+	 */
+	const CONSUMER_SECRET = '';
 
-    /**
-     * access token from apps.twitter.com under your access token heading
-     * @var string
-     */
-    const OAUTH_TOKEN = '';
+	/**
+	 * access token from apps.twitter.com under your access token heading
+	 * @var string
+	 */
+	const OAUTH_TOKEN = '';
 
-    /**
-     * access token secret from apps.twitter.com under your access token heading
-     * @var string
-     */
-    const OAUTH_TOKEN_SECRET = '';
-  }
+	/**
+	 * access token secret from apps.twitter.com under your access token heading
+	 * @var string
+	 */
+	const OAUTH_TOKEN_SECRET = '';
+}
 
-  class t7keysBitly {
-    /**
-     * bitly account login name
-     * @var string
-     */
-    const LOGIN = '';
+class t7keysBitly {
+	/**
+	 * bitly account login name
+	 * @var string
+	 */
+	const LOGIN = '';
 
-    /**
-     * bitly account api key
-     * @var string
-     */
-    const KEY = '';
-  }
-?>
+	/**
+	 * bitly account api key
+	 * @var string
+	 */
+	const KEY = '';
+}
+
+class t7keysCloudflare {
+	/**
+	 * email address of the cloudflare account
+	 * @var string
+	 */
+	const EMAIL = '';
+
+	/**
+	 * global api key from https://www.cloudflare.com/a/profile
+	 * @var string
+	 */
+	const GLOBAL_API_KEY = '';
+
+	/**
+	 * id returned from requesting https://api.cloudflare.com/client/v4/zones with email and global api key
+	 * @var string
+	 */
+	const ID = '';
+	const OWNER = '';  // probably don't need this
+}
--- tools/git-pull.js
+++ tools/git-pull.js
@@ -1,18 +1,24 @@
 $(function() {
-  $("a[href$='#pull']").click(function() {
-    $.post("?ajax=pull", {}, function(data, status, xhr) {
-      var result = $.parseJSON(xhr.responseText);
-      if(!result.fail) {
-        $(".actions").after("<pre><code>" + result.output + "</code></pre>");
-        var now = new Date();
-        var hour = now.getHours();
-        $(".actions").after("<h2>git pull returned " + result.retcode + " at "
-            + (hour == 0 ? 12 : hour > 12 ? hour - 12 : hour)
-            + (now.getMinutes() < 10 ? ":0" : ":") + now.getMinutes()
-            + (hour >= 12 ? " pm" : " am") + "</h2>");
-      } else
-        alert(result.message);
-    });
-    return false;
-  });
+	$("a[href$='#pull']").click(function() {
+		$.post("?ajax=pull", {}, function(data, status, xhr) {
+			var result = $.parseJSON(xhr.responseText);
+			if(!result.fail) {
+				if(result.cachedel && result.cachedel.length) {
+					$(".actions").after("<pre class=language-json><code class=language-json>" + result.cloudflare.text + "</code></pre>");
+					$(".actions").after("<h3>cloudflare returned " + result.cloudflare.code + "</h3>");
+					$(".actions").after("<ul><li>" . result.cachedel.join("</li><li>") . "</li></ul>");
+					$(".actions").after("<p>deleted " + result.cachedel.length + " files from cloudflare cache:</p>);
+				}
+				$(".actions").after("<pre><code>" + result.output + "</code></pre>");
+				var now = new Date();
+				var hour = now.getHours();
+				$(".actions").after("<h2>git pull returned " + result.retcode + " at "
+					+ (hour == 0 ? 12 : hour > 12 ? hour - 12 : hour)
+					+ (now.getMinutes() < 10 ? ":0" : ":") + now.getMinutes()
+					+ (hour >= 12 ? " pm" : " am") + "</h2>");
+			} else
+				alert(result.message);
+		});
+		return false;
+	});
 });
