41b528cb7d1663a611e85663be65ace8c551b748
misterhaan <misterhaan@users.noreply.github.com>
2018-08-21 12:55:49 -0500

    home page use api and vue

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 $(function() {
-	ko.applyBindings(window.ActivityVM = new ActivityViewModel(), $("#latestactivity")[0]);
-});
-
-function ActivityViewModel() {
-	var self = this;
-	this.activity = ko.observableArray([]);
-	this.latest = 0;
-	this.more = ko.observable(false);
-	this.loading = ko.observable(false);
-
-	this.Load = function() {
-		self.loading(true);
-		$.get("?ajax=activity", {before: self.latest}, function(result) {
-			if(result.fail)
-				alert(result.message);
-			else {
-				for(var a = 0; a < result.acts.length; a++)
-					self.activity.push(result.acts[a]);
-				self.latest = result.latest;
-				self.more(result.more);
+	var latestactivity = new Vue({
+		el: "#latestactivity",
+		data: {
+			activity: [],
+			latest: 0,
+			more: false,
+			loading: false
+		},
+		methods: {
+			Load: function() {
+				this.loading = true;
+				var self = this;
+				$.get("/api/activity/latest", {before: this.latest}, function(result) {
+					if(result.fail)
+						alert(result.message);
+					else {
+						self.activity = self.activity.concat(result.acts);
+						self.latest = result.latest;
+						self.more = result.more;
+					}
+					self.loading = false;
+				}, "json");
 			}
-			self.loading(false);
-		}, "json");
-	};
-	this.Load();
-}
+		}
+	});
+	latestactivity.Load();
+
+	var features = new Vue({
+		el: "#features",
+		data: {
+			features: [
+				{id: "bln", name: "blog", desc: "read the blog"},
+				{id: "album", name: "photo album", desc: "see my photos"},
+				{id: "guides", name: "guides", desc: "learn how i’ve done things"},
+				{id: "lego", name: "lego models", desc: "download instructions for custom lego models"},
+				{id: "art", name: "visual art", desc: "see sketches and digital artwork"},
+				{id: "pen", name: "stories", desc: "read short fiction and a poem"},
+				{id: "code", name: "software", desc: "download free software with source code"},
+				{id: "forum", name: "forums", desc: "join or start conversations"}
+			]
+		}
+	});
+});
--- index.php
+++ index.php
@@ -1,70 +1,16 @@
 <?php
-define('MAXITEMS', 9);
 require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/class/t7.php';
 
-if(isset($_GET['ajax'])) {
-	$ajax = new t7ajax();
-	switch($_GET['ajax']) {
-		case 'activity':
-			$before = isset($_GET['before']) && +$_GET['before'] ? +$_GET['before'] : false;
-			if($acts = t7contrib::GetAll($before, MAXITEMS)) {
-				$ajax->Data->acts = [];
-				$ajax->Data->latest = false;
-				while($act = $acts->fetch_object()) {
-					$ajax->Data->latest = $act->posted;
-					$act->posted = t7format::TimeTag('smart', $act->posted, t7format::DATE_LONG);
-					$act->prefix = t7contrib::Prefix($act->conttype);
-					$act->postfix = t7contrib::Postfix($act->conttype);
-					$act->hasmore += 0;  // convert to numeric
-					$ajax->Data->acts[] = $act;
-				}
-				$ajax->Data->more = t7contrib::More($ajax->Data->latest);
-			} else
-				$ajax->Fail('error looking up activity:  ' . $db->error);
-			break;
-	}
-	$ajax->Send();
-	die;
-}
-
-$html = new t7html(['ko' => true, 'rss' => ['title' => 'unifeed', 'url' => '/feed.rss']]);
+$html = new t7html(['vue' => true, 'rss' => ['title' => 'unifeed', 'url' => '/feed.rss']]);
 $html->Open('track7');
 ?>
 			<h1><img alt=track7 src="/images/track7.png"></h1>
 
 			<section id=features>
 				<nav>
-					<a href="/bln/" title="read the blog">
-						<img src="/bln/favicon.png" alt="">
-						blog
-					</a>
-					<a href="/album/" title="see my photos">
-						<img src="/album/favicon.png" alt="">
-						photo album
-					</a>
-					<a href="/guides/" title="learn how i’ve done things">
-						<img src="/guides/favicon.png" alt="">
-						guides
-					</a>
-					<a href="/lego/" title="download instructions for custom lego models">
-						<img src="/lego/favicon.png" alt="">
-						lego models
-					</a>
-					<a href="/art/" title="see sketches and digital artwork">
-						<img src="/art/favicon.png" alt="">
-						visual art
-					</a>
-					<a href="/pen/" title="read short fiction and a poem">
-						<img src="/pen/favicon.png" alt="">
-						stories
-					</a>
-					<a href="/code/" title="download free software with source code">
-						<img src="/code/favicon.png" alt="">
-						software
-					</a>
-					<a href="/forum/" title="join or start conversations">
-						<img src="/forum/favicon.png" alt="">
-						forums
+					<a v-for="feat in features" :href="'/' + feat.id + '/'" :title=desc>
+						<img :src="'/' + feat.id + '/favicon.png'" alt="">
+						{{feat.name}}
 					</a>
 <?php
 if($user->IsAdmin()) {
@@ -86,34 +32,26 @@ if($user->IsAdmin()) {
 }
 ?>
 			<div id=latestactivity>
-				<!-- ko foreach: activity -->
-				<article class="activity" data-bind="css: conttype">
-					<div class=whatwhen data-bind="attr: {title: conttype + ' at ' + posted.title}">
-						<time data-bind="html: posted.display, attr: {datetime: posted.datetime}"></time>
+				<article class="activity" v-for="act in activity" :class=act.conttype>
+					<div class=whatwhen :title="act.conttype + ' at ' + act.posted.title">
+						<time v-html=act.posted.display :datetime=act.posted.datetime></time>
 					</div>
 					<div>
 						<h2>
-							<span data-bind="text: prefix"></span>
-							<a data-bind="text: title, attr: {href: url}"></a>
-							<span data-bind="text: postfix"></span>
+							<span>{{act.prefix}}</span>
+							<a :href=act.url>{{act.title}}</a>
+							<span>{{act.postfix}}</span>
 							by
-							<!-- ko if: username -->
-							<a data-bind="text: displayname || username, attr: {href: '/user/' + username + '/', title: 'view ' + (displayname || username) + '’s profile'}"></a>
-							<!-- /ko -->
-							<!-- ko if: !username && authorurl -->
-							<a data-bind="text: authorname, attr: {href: authorurl}"></a>
-							<!-- /ko -->
-							<!-- ko if: !username && !authorurl -->
-							<span data-bind="text: authorname"></span>
-							<!-- /ko -->
+							<a v-if=act.username :href="'/user/' + act.username + '/'" :title="'view ' + (act.displayname || act.username) + '’s profile'">{{act.displayname || act.username}}</a>
+							<a v-if="!act.username && act.authorurl" :href=act.authorurl>{{act.authorname}}</a>
+							<span v-if="!act.username && !act.authorurl">{{act.authorname}}</span>
 						</h2>
-						<div class=summary data-bind="html: preview"></div>
-						<p class=readmore data-bind="visible: hasmore"><a class=continue data-bind="attr: {href: url}">read more</a></p>
+						<div class=summary v-html=act.preview></div>
+						<p v-if=act.hasmore class=readmore><a class=continue :href=act.url>read more</a></p>
 					</div>
 				</article>
-				<!-- /ko -->
-				<p class=loading data-bind="visible: loading">loading activity...</p>
-				<p class="more calltoaction" data-bind="visible: more() && !loading()"><a class="action get" href="#activity" data-bind="click: Load">show more activity</a></p>
+				<p class=loading v-if=loading>loading activity...</p>
+				<p class="more calltoaction" v-if="more && !loading"><a class="action get" href="/api/activity/latest" v-on:click.prevent=Load>show more activity</a></p>
 			</div>
 <?php
 $html->Close();
