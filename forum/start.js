e6db1c8bdcfb0acda9d1ceb885736759e4b672cd
misterhaan <misterhaan@users.noreply.github.com>
2018-11-08 20:45:34 -0600

    remove old ko code

--- forum/start.js
+++ forum/start.js
@@ -34,24 +34,3 @@ $(function() {
 		}
 	});
 });
-
-function StartDiscussionViewModel() {
-	var self = this;
-	this.name = ko.observable("");
-	this.contact = ko.observable("");
-	this.title = ko.observable("");
-	this.tags = ko.observableArray([]);
-	this.message = ko.observable("");
-	this.saving = ko.observable(false);
-
-	this.Post = function() {
-		self.saving(true);
-		$.post("?ajax=post", {name: self.name(), contact: self.contact(), title: self.title(), taglist: self.tags().join(","), markdown: self.message()}, function(result) {
-			if(result.fail) {
-				alert(result.message);
-				self.saving(false);
-			} else
-				window.location.href = result.url;
-		});
-	};
-}
