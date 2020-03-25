edbdd88d3b0aad622bac9df16b645f1bf6aa14dd
misterhaan <misterhaan@track7.org>
2020-03-25 14:04:21 -0500

    update copyright year

--- etc/class/t7html.php
+++ etc/class/t7html.php
@@ -309,7 +309,7 @@ class t7html {
 			<a href="/feed.rss" title="add track7 activity to your feed reader">rss</a>
 			<a href="https://twitter.com/track7feed" title="follow track7 on twitter">twitter</a>
 			<a href="https://github.com/misterhaan/track7.org/blob/master<?=$_SERVER['SCRIPT_NAME']; ?>?ts=2" title="view the php source for this page on github">php source</a>
-			<div id=copyright>© 1996 - 2019 track7 — <a href="/fewrights.php">few rights reserved</a></div>
+			<div id=copyright>© 1996 - 2020 track7 — <a href="/fewrights.php">few rights reserved</a></div>
 		</footer>
 	</body>
 </html>
