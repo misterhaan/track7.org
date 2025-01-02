a14bbae49aeca7d2c6d72174c8652ab25ef3f316
misterhaan <misterhaan@track7.org>
2025-01-01 20:28:24 -0600

    update copyright year

--- etc/class/page.php
+++ etc/class/page.php
@@ -6,7 +6,7 @@ require_once 'environment.php';
  */
 abstract class Page extends Responder {
 	private const siteTitle = 'track7';
-	private const copyrightEndYear = 2024;
+	private const copyrightEndYear = 2025;
 
 	protected static string $title = self::siteTitle;
 	protected static string $bodytype = 'text';
