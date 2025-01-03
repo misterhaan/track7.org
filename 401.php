b0c4ee23106d8f1dfba4a3ab7ed314e399a8c941
misterhaan <misterhaan@track7.org>
2025-01-03 17:43:26 -0600

    migrate static pages

--- 401.php
+++ 401.php
@@ -1,17 +1,23 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/class/t7.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/class/page.php';
 
-$html = new t7html([]);
-$html->Open('401 authorization required');
+class AuthRequired extends Page {
+	public function __construct() {
+		parent::__construct('401 authorization required');
+	}
+
+	protected static function MainContent(): void {
 ?>
-			<h1>401 you are not me</h1>
+		<h1>401 you are not me</h1>
 
-			<p>
-				the page you requested is only for me, and since you did not prove that
-				you are me, you cannot see it.
-			</p>
+		<p>
+			the page you requested is only for me, and since you did not prove that
+			you are me, you cannot see it.
+		</p>
 
-			<p class=calltoaction><a href="/" class=action>go to the track7 front page</a></p>
+		<p class=calltoaction><a href="/" class=action>go to the track7 front page</a></p>
 
 <?php
-$html->Close();
+	}
+}
+new AuthRequired();
--- what.php
+++ what.php
@@ -1,100 +1,108 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/class/t7.php';
-$html = new t7html([]);
-$html->Open('about track7');
+require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/class/page.php';
+
+class What extends Page {
+	public function __construct() {
+		parent::__construct('about track7');
+	}
+
+	protected static function MainContent(): void {
 ?>
-			<h1>what’s all this then?</h1>
+		<h1>what’s all this then?</h1>
 
-			<p>
-				trying to figure out what track7 is all about?  the short answer is it’s
-				my personal website, and i am <a href="/user/misterhaan/">misterhaan</a>.
-				as it’s a personal site, i put whatever i want on it.  you’re likely to
-				come across various things i’m interested in or have created.
-			</p>
+		<p>
+			trying to figure out what track7 is all about? the short answer is it’s
+			my personal website, and i am <a href="/user/misterhaan/">misterhaan</a>.
+			as it’s a personal site, i put whatever i want on it. you’re likely to
+			come across various things i’m interested in or have created.
+		</p>
 
-			<h2>track7</h2>
-			<p>
-				track7 has been on the internet since 1996, though back then it was “the
-				site of cool stuffs” since i hadn’t come up with a real name.  since
-				most good names have at least two meanings, you can bet track7 has just
-				that:  the album <em>fathom</em> by mortal has 7 seconds of silence for
-				its 7th track, then the hidden track at the end starts with “welcome to
-				track seven,” which for a time i had actually play when you first
-				visited track7.  the other meaning is that at the time i chose the name
-				i had noticed that on most albums i listened to, the seventh track was
-				at least a very good song if not the best on the album.  i guess i
-				wanted to be the best site on the internet, or at least a very good one :).
-			</p>
-			<p>
-				track7 started out on free hosting and spent many years costing me
-				nothing at all to keep up.  for a long time track7 was a subdomain of
-				various free hosts, with a redirection service pointing track7.vze.com
-				pointing to the latest one as i had to move due to free service ending
-				or me wanting more features.  eventually i started to have money and so
-				started paying for hosting and also bought the domain track7.org (at the
-				time both track7.com and track7.net, the one i really wanted, were
-				taken).  i’ve had track7.org for long enough now though that if i did
-				get one of the others i’d just redirect it here.
-			</p>
-			<p>
-				track7 is a large site with varied and somewhat unrelated contents while
-				most of the internet is more focused sites.  parts of track7 would
-				probably do better on their own, but since it’s a personal site it
-				doesn’t matter how popular it gets.  if people find it and something is
-				interesting or useful that’s great, but the point of the site is for me
-				to practice web development and have a place to keep all my stuff.
-			</p>
+		<h2>track7</h2>
+		<p>
+			track7 has been on the internet since 1996, though back then it was “the
+			site of cool stuffs” since i hadn’t come up with a real name. since
+			most good names have at least two meanings, you can bet track7 has just
+			that: the album <em>fathom</em> by mortal has 7 seconds of silence for
+			its 7th track, then the hidden track at the end starts with “welcome to
+			track seven,” which for a time i had actually play when you first
+			visited track7. the other meaning is that at the time i chose the name
+			i had noticed that on most albums i listened to, the seventh track was
+			at least a very good song if not the best on the album. i guess i
+			wanted to be the best site on the internet, or at least a very good one :).
+		</p>
+		<p>
+			track7 started out on free hosting and spent many years costing me
+			nothing at all to keep up. for a long time track7 was a subdomain of
+			various free hosts, with a redirection service pointing track7.vze.com
+			pointing to the latest one as i had to move due to free service ending
+			or me wanting more features. eventually i started to have money and so
+			started paying for hosting and also bought the domain track7.org (at the
+			time both track7.com and track7.net, the one i really wanted, were
+			taken). i’ve had track7.org for long enough now though that if i did
+			get one of the others i’d just redirect it here.
+		</p>
+		<p>
+			track7 is a large site with varied and somewhat unrelated contents while
+			most of the internet is more focused sites. parts of track7 would
+			probably do better on their own, but since it’s a personal site it
+			doesn’t matter how popular it gets. if people find it and something is
+			interesting or useful that’s great, but the point of the site is for me
+			to practice web development and have a place to keep all my stuff.
+		</p>
 
-			<h2>misterhaan</h2>
-			<p>
-				the name <em>misterhaan</em> starts with mister for a reason similar to
-				why mr. t has the mr in front — so people have to call me mister.
-				during high school i worked at a dairy queen and at first my nametag
-				said “trainee” under my name, which i thought was a nice excuse for
-				being a little slow and not knowing much.  once i did know things though
-				(even after my nametag no longer said “trainee”) i noticed people
-				sometimes didn’t believe me when i told them we weren’t allowed to put
-				certain toppings in blizzards.  one slow day i got out the labeler and
-				made a new label for my nametag which had “mr.” in front of my name.
-				after that people believed whatever i told them.  so the first part of
-				the name <em>misterhaan</em> is for credibility and respect.  the second
-				part is a name i made up for a druid character in some old rpg i don’t
-				play anymore.  i just liked how it looks with the double <em>a</em> and
-				how it sounds (it’s like han solo from star wars).  i don’t like to use
-				my real name on parts of the internet anyone can see since i’m not into
-				the idea of some crazy person tracking me down, so you shouldn’t see
-				much indication of who i am or where i live.  if you don’t already know
-				that stuff, you can just know me as misterhaan, or shorten it to haan if
-				you prefer.
-			</p>
-			<p>
-				i have a degree in electrical engineering and work as a developer at a
-				software company, so i’m what people typically think of when they
-				picture a geek (though hopefully a little better looking).  i know both
-				the hardware and software sides of a computer pretty well, which is why
-				track7 uses mostly code i personally wrote and i have been building my
-				own computers for longer than i’ve had a website.  you can download some
-				of my software in <a href="/code/">the analag underground</a> section.
-				you’ll also see i sometimes post in <a href="/bln/">my blog</a> about
-				web and desktop development.  i also sometimes write up more formal
-				<a href="/guides/">guides</a> (some sites call them tutorials) to help
-				other people figure out how to do things that may have taken me a while
-				to figure out.
-			</p>
-			<p>
-				when i do something or go somewhere interesting, i sometimes take
-				pictures and add my favorites to the <a href="/album/">photo album</a>.
-				each photo has a description and / or story with it.  though i don’t
-				find the motivation to do it much anymore, i used to write
-				<a href="/pen/">short stories</a> and have also drawn a few
-				<a href="/art/">things</a>.  i still have my lego collection from
-				growing up, and i started putting the stuff i’ve built on my own into
-				ldraw to generate instructions so you can build
-				<a href="/lego/">my lego models</a> too.
-			</p>
-			<p>
-				now that you know a little bit about me, why not sign in and tell me a
-				little about yourself?
-			</p>
+		<h2>misterhaan</h2>
+		<p>
+			the name <em>misterhaan</em> starts with mister for a reason similar to
+			why mr. t has the mr in front — so people have to call me mister.
+			during high school i worked at a dairy queen and at first my nametag
+			said “trainee” under my name, which i thought was a nice excuse for
+			being a little slow and not knowing much. once i did know things though
+			(even after my nametag no longer said “trainee”) i noticed people
+			sometimes didn’t believe me when i told them we weren’t allowed to put
+			certain toppings in blizzards. one slow day i got out the labeler and
+			made a new label for my nametag which had “mr.” in front of my name.
+			after that people believed whatever i told them. so the first part of
+			the name <em>misterhaan</em> is for credibility and respect. the second
+			part is a name i made up for a druid character in some old rpg i don’t
+			play anymore. i just liked how it looks with the double <em>a</em> and
+			how it sounds (it’s like han solo from star wars). i don’t like to use
+			my real name on parts of the internet anyone can see since i’m not into
+			the idea of some crazy person tracking me down, so you shouldn’t see
+			much indication of who i am or where i live. if you don’t already know
+			that stuff, you can just know me as misterhaan, or shorten it to haan if
+			you prefer.
+		</p>
+		<p>
+			i have a degree in electrical engineering and work as a developer at a
+			software company, so i’m what people typically think of when they
+			picture a geek (though hopefully a little better looking). i know both
+			the hardware and software sides of a computer pretty well, which is why
+			track7 uses mostly code i personally wrote and i have been building my
+			own computers for longer than i’ve had a website. you can download some
+			of my software in <a href="/code/">the analag underground</a> section.
+			you’ll also see i sometimes post in <a href="/bln/">my blog</a> about
+			web and desktop development. i also sometimes write up more formal
+			<a href="/guides/">guides</a> (some sites call them tutorials) to help
+			other people figure out how to do things that may have taken me a while
+			to figure out.
+		</p>
+		<p>
+			when i do something or go somewhere interesting, i sometimes take
+			pictures and add my favorites to the <a href="/album/">photo album</a>.
+			each photo has a description and / or story with it. though i don’t
+			find the motivation to do it much anymore, i used to write
+			<a href="/pen/">short stories</a> and have also drawn a few
+			<a href="/art/">things</a>. i still have my lego collection from
+			growing up, and i started putting the stuff i’ve built on my own into
+			ldraw to generate instructions so you can build
+			<a href="/lego/">my lego models</a> too.
+		</p>
+		<p>
+			now that you know a little bit about me, why not sign in and tell me a
+			little about yourself?
+		</p>
+
 <?php
-$html->Close();
+	}
+}
+new What();
